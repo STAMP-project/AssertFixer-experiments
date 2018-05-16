5d378ad2160dde99770009951d6a232602e5f6a9
Luc Esape <luc.esape@gmail.com>
2018-05-16 20:50:49 +0200

    Bug commit from the following repository Raaycc/inglesapp This bug commit is a reflect of source code from: http://github.com/Raaycc/inglesapp/commit/7897da902ecc993f24eaed6b61cbf48c0b04811a.

---
+++ src/main/java/br/edu/fapce/nexti/repository/BibliotecaRepository.java
@@ -0,0 +1,10 @@
+package br.edu.fapce.nexti.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.edu.fapce.nexti.model.Biblioteca;
+
+public interface BibliotecaRepository extends CrudRepository<Biblioteca, Long>{
+
+}
+
---
+++ src/main/java/br/edu/fapce/nexti/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package br.edu.fapce.nexti.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.edu.fapce.nexti.model.Usuario;
+
+public interface UsuarioRepository extends  CrudRepository<Usuario, Long> {
+
+}
