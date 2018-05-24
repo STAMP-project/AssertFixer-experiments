3a92f5a12a860d9beb07b2e15150b4c181c7f3e4
Luc Esape <luc.esape@gmail.com>
2018-05-25 00:06:09 +0200

    Bug commit from the following repository codeu-2018-team31/codeu_project_2018 This bug commit is a reflect of source code from: http://github.com/codeu-2018-team31/codeu_project_2018/commit/0703a15133f8bd3d012ae847156e79524f39585c.

---
+++ src/main/java/codeu/model/data/Tag.java
@@ -0,0 +1,82 @@
+package codeu.model.data;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/** Class representing a tag. */
+public class Tag {
+  private final UUID id;
+  private final Conversation conversation;
+  private final String tag;
+  private final Instant creation;
+  private List<Conversation> conversations;
+
+  /**
+   * Constructs a new Tag.
+   *
+   * @param id the ID of this Tag
+   * @param conversation The first Conversation this Tag is associated with.
+   * @param conversations the IDs of the Conversations this Tag belongs to
+   * @param tag the content of this Tag
+   * @param creation the creation time of this Tag
+   */
+
+  public Tag(UUID id, Conversation conversation, String tag, Instant creation) {
+
+    this.id = id;
+    this.conversation = conversation;
+    this.tag = tag;
+    this.creation = creation;
+    this.conversations = new ArrayList<>();
+    this.conversations.add(conversation);
+  }
+
+  /** Adds a Conversation that has this Tag */
+  public void addConversation(Conversation conversation) {
+    conversations.add(conversation);
+  }
+
+  /** Gets all Conversations that have this Tag */
+  public List<Conversation> getConversations() {
+    return conversations;
+  }
+
+  /** Gets the Conversation that this Tag was created with*/
+  public Conversation getConversation() {
+    return conversation;
+  }
+
+  /** Returns the ID of this Tag. */
+  public UUID getId() {
+    return id;
+  }
+
+  /** Returns the content of this Tag. */
+  public String getTag() {
+    return tag;
+  }
+
+  /** Returns the creation time of this Tag. */
+  public Instant getCreationTime() {
+    return creation;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Tag)) {
+      return false;
+    }
+
+    Tag other = (Tag) o;
+    if (other.getTag().equals(tag)) {
+      return true;
+    }
+    return false;
+  }
+}
+
---
+++ src/test/java/codeu/model/data/TagTest.java
@@ -0,0 +1,25 @@
+package codeu.model.data;
+
+import java.time.Instant;
+import java.util.UUID;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TagTest {
+
+  @Test
+  public void testCreate() {
+    UUID id = UUID.randomUUID();
+    UUID conv_id = UUID.randomUUID();
+    String tag = "test_tag";
+    Instant creation = Instant.now();
+    Conversation conversation = new Conversation(conv_id, UUID.randomUUID(), "test", creation);
+
+    Tag test_tag = new Tag(id, conversation, tag, creation);
+    Assert.assertEquals(id, test_tag.getId());
+    Assert.assertEquals(conv_id, test_tag.getConversation().getId());
+    Assert.assertEquals(tag, test_tag.getTag());
+    Assert.assertEquals(creation, test_tag.getCreationTime());
+  }
+}
+
