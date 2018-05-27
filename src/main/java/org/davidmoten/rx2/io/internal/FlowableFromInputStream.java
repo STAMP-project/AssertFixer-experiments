cdcdefff1d92f1bb5d31b6a80406ce15af7b3bba
Luc Esape <luc.esape@gmail.com>
2018-05-27 07:41:05 +0200

    Bug commit from the following repository davidmoten/rxjava2-http This bug commit is a reflect of source code from: http://github.com/davidmoten/rxjava2-http/commit/bf2e2da6ee764d0f4edbaf78a1f970547fe868a6.

---
+++ src/main/java/org/davidmoten/rx2/io/internal/FlowableFromInputStream.java
@@ -0,0 +1,184 @@
+package org.davidmoten.rx2.io.internal;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import io.reactivex.Flowable;
+import io.reactivex.exceptions.Exceptions;
+import io.reactivex.functions.BiConsumer;
+import io.reactivex.internal.subscriptions.SubscriptionHelper;
+import io.reactivex.internal.util.BackpressureHelper;
+import io.reactivex.internal.util.EmptyComponent;
+import io.reactivex.plugins.RxJavaPlugins;
+
+public final class FlowableFromInputStream extends Flowable<ByteBuffer> {
+
+    private final InputStream in;
+    private final BiConsumer<Long, Long> requester;
+
+    public FlowableFromInputStream(InputStream in, BiConsumer<Long, Long> requester) {
+        this.in = in;
+        this.requester = requester;
+    }
+
+    @Override
+    protected void subscribeActual(Subscriber<? super ByteBuffer> subscriber) {
+        FromStreamSubscription subscription = new FromStreamSubscription(in, requester, subscriber);
+        subscription.start();
+    }
+
+    private static final class FromStreamSubscription extends AtomicInteger implements Subscription {
+
+        private static final long serialVersionUID = 5917186677331992560L;
+
+        private final InputStream in;
+        private final Subscriber<? super ByteBuffer> child;
+        private final AtomicLong requested;
+        private long id;
+
+        private volatile boolean cancelled;
+        private final BiConsumer<Long, Long> requester;
+        private long emitted;
+        private int length = 0;
+        private byte[] buffer;
+        private int bufferIndex;
+
+        FromStreamSubscription(InputStream in, BiConsumer<Long, Long> requester, Subscriber<? super ByteBuffer> child) {
+            this.in = in;
+            this.requester = requester;
+            this.child = child;
+            this.requested = new AtomicLong();
+        }
+
+        public void start() {
+            try {
+                id = Util.readLong(in);
+            } catch (Throwable e) {
+                Exceptions.throwIfFatal(e);
+                closeStreamSilently();
+                child.onSubscribe(EmptyComponent.INSTANCE);
+                child.onError(e);
+                return;
+            }
+            child.onSubscribe(this);
+            drain();
+        }
+
+        @Override
+        public void request(long n) {
+            if (SubscriptionHelper.validate(n)) {
+                BackpressureHelper.add(requested, n);
+                try {
+                    requester.accept(id, n);
+                } catch (Exception e) {
+                    Exceptions.throwIfFatal(e);
+                    closeStreamSilently();
+                    child.onError(e);
+                    return;
+                }
+                drain();
+            }
+        }
+
+        private void drain() {
+            if (getAndIncrement() == 0) {
+                int missed = 1;
+                while (true) {
+                    long r = requested.get();
+                    long e = emitted;
+                    while (e != r) {
+                        if (tryCancelled()) {
+                            return;
+                        }
+                        // read some more
+                        if (buffer == null) {
+                            try {
+                                length = Util.readInt(in);
+                            } catch (IOException e1) {
+                                emitError(e1);
+                                return;
+                            }
+                            if (length == Integer.MIN_VALUE) {
+                                closeStreamSilently();
+                                child.onComplete();
+                                return;
+                            }
+                            buffer = new byte[Math.abs(length)];
+                            bufferIndex = 0;
+                        }
+                        try {
+                            int count = in.read(buffer, bufferIndex, Math.abs(length) - bufferIndex);
+                            if (count == -1) {
+                                emitError(new EOFException("encountered EOF before expected length was read"));
+                                return;
+                            }
+                            bufferIndex += count;
+                            if (bufferIndex == Math.abs(length)) {
+                                if (length < 0) {
+                                    String t = new String(buffer, 0, -length, StandardCharsets.UTF_8);
+                                    buffer = null;
+                                    child.onError(new RuntimeException(t));
+                                    return;
+                                } else {
+                                    child.onNext(ByteBuffer.wrap(buffer, 0, length));
+                                    buffer = null;
+                                    e++;
+                                }
+                            }
+                        } catch (Throwable ex) {
+                            emitError(ex);
+                            return;
+                        }
+                    }
+                    emitted = e;
+                    missed = addAndGet(-missed);
+                    if (missed == 0) {
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void emitError(Throwable e) {
+            closeStreamSilently();
+            child.onError(e);
+        }
+
+        private boolean tryCancelled() {
+            if (cancelled) {
+                closeStreamSilently();
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        private void closeStreamSilently() {
+            Util.close(in);
+        }
+
+        @Override
+        public void cancel() {
+            cancelled = true;
+            try {
+                // a negative request cancels the stream
+                requester.accept(id, -1L);
+            } catch (Exception e) {
+                Exceptions.throwIfFatal(e);
+                closeStreamSilently();
+                RxJavaPlugins.onError(e);
+                return;
+            }
+        }
+
+    }
+
+}
---
+++ src/main/java/org/davidmoten/rx2/io/internal/Server.java
@@ -0,0 +1,234 @@
+package org.davidmoten.rx2.io.internal;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscription;
+
+import io.reactivex.FlowableSubscriber;
+import io.reactivex.Scheduler;
+import io.reactivex.Scheduler.Worker;
+import io.reactivex.SingleObserver;
+import io.reactivex.SingleSource;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.internal.fuseable.SimplePlainQueue;
+import io.reactivex.internal.queue.SpscLinkedArrayQueue;
+import io.reactivex.plugins.RxJavaPlugins;
+
+public final class Server {
+
+    private Server() {
+        // prevent instantiation
+    }
+
+    public static void handle(Publisher<? extends ByteBuffer> flowable, SingleSource<OutputStream> out, Runnable done,
+            long id, Scheduler requestScheduler, Consumer<Subscription> subscription) {
+        // when first request read (8 bytes) subscribe to Flowable
+        // and output to OutputStream on scheduler
+        HandlerSubscriber subscriber = new HandlerSubscriber(out, done, id, requestScheduler);
+        try {
+            subscription.accept(subscriber);
+        } catch (Exception e) {
+            throw new RuntimeException("subscription consumer threw", e);
+        }
+        flowable.subscribe(subscriber);
+    }
+
+    private static final class HandlerSubscriber extends AtomicInteger
+            implements FlowableSubscriber<ByteBuffer>, Subscription, SingleObserver<OutputStream> {
+
+        private static final long serialVersionUID = 1331107616659478552L;
+
+        private final SingleSource<OutputStream> outSource;
+        private OutputStream out;
+        private final Runnable completion;
+        private final long id;
+        private final Worker worker;
+        private Subscription parent;
+        private SimplePlainQueue<ByteBuffer> queue;
+        private volatile boolean finished;
+        private Throwable error;
+        private volatile boolean cancelled;
+        private Disposable disposable;
+
+        HandlerSubscriber(SingleSource<OutputStream> outSource, Runnable completion, long id,
+                Scheduler requestScheduler) {
+            this.outSource = outSource;
+            this.completion = completion;
+            this.id = id;
+            this.worker = requestScheduler.createWorker();
+            this.queue = new SpscLinkedArrayQueue<>(16);
+        }
+
+        @Override
+        public void onSubscribe(Subscription parent) {
+            this.parent = parent;
+            outSource.subscribe(this);
+        }
+
+        // SingleObserver for outSource
+
+        @Override
+        public void onSubscribe(Disposable d) {
+            disposable = d;
+        }
+
+        @Override
+        public void onSuccess(OutputStream os) {
+            this.out = os;
+            try {
+                out.write(Util.toBytes(id));
+                out.flush();
+            } catch (IOException e) {
+                error = e;
+                finished = true;
+            }
+            drain();
+        }
+
+        @Override
+        public void request(long n) {
+            worker.schedule(() -> {
+                parent.request(n);
+                drain();
+            });
+        }
+
+        @Override
+        public void cancel() {
+            try {
+                cancelled = true;
+                disposable.dispose();
+                parent.cancel();
+                worker.dispose();
+            } finally {
+                Util.close(out);
+            }
+            completion.run();
+        }
+
+        // end of SingleObserver
+
+        @Override
+        public void onNext(ByteBuffer bb) {
+            queue.offer(bb);
+            drain();
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            error = e;
+            finished = true;
+            drain();
+        }
+
+        @Override
+        public void onComplete() {
+            finished = true;
+            drain();
+        }
+
+        private void drain() {
+            if (getAndIncrement() == 0) {
+                int missed = 1;
+                while (true) {
+                    while (true) {
+                        if (cancelled) {
+                            parent.cancel();
+                            queue.clear();
+                            error = null;
+                            worker.dispose();
+                            completion.run();
+                            return;
+                        }
+                        boolean d = finished;
+                        ByteBuffer b = queue.poll();
+                        if (b != null) {
+                            try {
+                                writeOnNext(b);
+                            } catch (Throwable e) {
+                                parent.cancel();
+                                queue.clear();
+                                worker.dispose();
+                                if (!cancelled) {
+                                    writeError(e);
+                                }
+                                return;
+                            }
+                        } else if (d) {
+                            Throwable err = error;
+                            if (err != null) {
+                                error = null;
+                                parent.cancel();
+                                queue.clear();
+                                worker.dispose();
+                                writeError(err);
+                                completion.run();
+                                return;
+                            } else {
+                                doOnComplete();
+                                completion.run();
+                                return;
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+                    missed = addAndGet(-missed);
+                    if (missed == 0) {
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void doOnComplete() {
+            try {
+                // send the bytes -128, 0, 0, 0 to indicate completion
+                writeInt(out, Integer.MIN_VALUE);
+                out.flush();
+            } catch (IOException e) {
+                RxJavaPlugins.onError(e);
+            } finally {
+                Util.close(out);
+            }
+        }
+
+        private void writeError(Throwable err) {
+            try {
+                // set initial size to cover size of most stack traces
+                NoCopyByteArrayOutputStream bytes = new NoCopyByteArrayOutputStream(4096);
+                err.printStackTrace(new PrintStream(bytes, true, "UTF-8"));
+                bytes.close();
+
+                // mark as error by reporting length as negative
+                writeInt(out, -bytes.size());
+                bytes.write(out);
+                out.flush();
+            } catch (IOException e) {
+                RxJavaPlugins.onError(e);
+            } finally {
+                Util.close(out);
+            }
+        }
+
+        private void writeOnNext(ByteBuffer b) throws IOException {
+            writeInt(out, b.remaining());
+            out.write(Util.toBytes(b));
+            out.flush();
+        }
+    }
+
+    private static void writeInt(OutputStream out, int v) throws IOException {
+        out.write((v >>> 24) & 0xFF);
+        out.write((v >>> 16) & 0xFF);
+        out.write((v >>> 8) & 0xFF);
+        out.write((v >>> 0) & 0xFF);
+    }
+
+}
