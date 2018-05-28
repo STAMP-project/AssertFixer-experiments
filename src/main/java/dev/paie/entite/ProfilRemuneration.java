3eeab76a31b919f87457cb0db9b0ca1f03155785
Luc Esape <luc.esape@gmail.com>
2018-05-28 15:14:18 +0200

    Bug commit from the following repository dta-sherlock/sirh-gestion-paie This bug commit is a reflect of source code from: http://github.com/dta-sherlock/sirh-gestion-paie/commit/4d54871ccacf4937bd0bb2f59f710eb53700e50e.

---
+++ src/main/java/dev/paie/entite/ProfilRemuneration.java
@@ -0,0 +1,72 @@
+package dev.paie.entite;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "profilRemuneration")
+public class ProfilRemuneration {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column
+	private String code;
+
+	@Transient
+	private List<Cotisation> cotisationsNonImposables;
+
+	@Transient
+	private List<Cotisation> cotisationsImposables;
+
+	@Transient
+	private List<Avantage> avantages;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public List<Cotisation> getCotisationsNonImposables() {
+		return cotisationsNonImposables;
+	}
+
+	public void setCotisationsNonImposables(List<Cotisation> cotisationsNonImposables) {
+		this.cotisationsNonImposables = cotisationsNonImposables;
+	}
+
+	public List<Cotisation> getCotisationsImposables() {
+		return cotisationsImposables;
+	}
+
+	public void setCotisationsImposables(List<Cotisation> cotisationsImposables) {
+		this.cotisationsImposables = cotisationsImposables;
+	}
+
+	public List<Avantage> getAvantages() {
+		return avantages;
+	}
+
+	public void setAvantages(List<Avantage> avantages) {
+		this.avantages = avantages;
+	}
+
+}
