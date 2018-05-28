2510bf7a34a4deb660734d9ce68133f7b86897cc
Luc Esape <luc.esape@gmail.com>
2018-05-28 23:51:22 +0200

    Bug commit from the following repository themarkiron23/themarkironApiSpring This bug commit is a reflect of source code from: http://github.com/themarkiron23/themarkironApiSpring/commit/d1420736a133fa871aea73435d6dae74a444f524.

---
+++ src/main/java/br/com/pointsmart/api/SpringSmartPointApplication.java
@@ -0,0 +1,12 @@
+package br.com.pointsmart.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSmartPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSmartPointApplication.class, args);
+	}
+}
