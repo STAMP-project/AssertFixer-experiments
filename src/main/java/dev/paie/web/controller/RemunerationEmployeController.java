b86f48839a33710a62158fbac9d25504ae341da2
Luc Esape <luc.esape@gmail.com>
2018-05-29 14:10:23 +0200

    Bug commit from the following repository dta-sherlock/sirh-gestion-paie This bug commit is a reflect of source code from: http://github.com/dta-sherlock/sirh-gestion-paie/commit/716bf06051536110f700d1c6ed1d4a349af71396.

---
+++ src/main/java/dev/paie/web/controller/RemunerationEmployeController.java
@@ -0,0 +1,66 @@
+package dev.paie.web.controller;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import dev.paie.entite.Entreprise;
+import dev.paie.entite.Grade;
+import dev.paie.entite.ProfilRemuneration;
+import dev.paie.entite.RemunerationEmploye;
+import dev.paie.repository.EntrepriseRepository;
+import dev.paie.repository.GradeRepository;
+import dev.paie.repository.ProfilRemunerationRepository;
+import dev.paie.repository.RemunerationEmployeRepository;
+
+@Controller
+@RequestMapping("/employes")
+public class RemunerationEmployeController {
+
+	@Autowired
+	EntrepriseRepository eRepo;
+	@Autowired
+	ProfilRemunerationRepository prRepo;
+	@Autowired
+	GradeRepository gRepo;
+	@Autowired
+	RemunerationEmployeRepository reRepo;
+
+	@RequestMapping(method = RequestMethod.GET, path = "/creer")
+	public String creerEmploye(Model model) {
+		RemunerationEmploye remuneration = new RemunerationEmploye();
+		model.addAttribute("remuneration", remuneration);
+
+		List<Entreprise> entreprises = eRepo.findAll();
+		model.addAttribute("entreprises", entreprises);
+
+		List<ProfilRemuneration> profils = prRepo.findAll();
+		model.addAttribute("profils", profils);
+
+		List<Grade> grades = gRepo.findAll();
+		model.addAttribute("grades", grades);
+
+		return "employes/creerEmploye";
+	}
+
+	@RequestMapping(method = RequestMethod.POST, path = "/creer")
+	public String submitForm(@ModelAttribute("remuneration") RemunerationEmploye remuneration) {
+		remuneration.setHeureCreation(LocalDateTime.now());
+		remuneration.setHeureCreationFormat();
+		reRepo.save(remuneration);
+		return "redirect:/mvc/employes/lister";
+	}
+
+	@RequestMapping(method = RequestMethod.GET, path = "/lister")
+	public String listerEmploye(Model model) {
+		List<RemunerationEmploye> remunerations = reRepo.findAll();
+		model.addAttribute("remunerations", remunerations);
+		return "employes/listerEmploye";
+	}
+}
