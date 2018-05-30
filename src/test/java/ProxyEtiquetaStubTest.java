dfe29e3b0e4d10bf09debb31a90080b1b7df11aa
Luc Esape <luc.esape@gmail.com>
2018-05-30 03:52:50 +0200

    Bug commit from the following repository MatiasDurbano/re This bug commit is a reflect of source code from: http://github.com/MatiasDurbano/re/commit/7412ad1c0baa39c9d560604a781b62812c551622.

---
+++ src/test/java/ProxyEtiquetaStubTest.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import Etiquetado.ProxyEtiquetado;
+import Etiquetado.ProxyEtiquetadoStub;
+import modelo.Etiqueta;
+import modelo.Ingrediente;
+
+public class ProxyEtiquetaStubTest {
+	
+	@Test
+	public void getEtiqueta() {
+		ProxyEtiquetado proxyEtiquetado = new ProxyEtiquetadoStub();
+		Assert.assertEquals(new Etiqueta("celiaco"), proxyEtiquetado.getEtiqueta(new Ingrediente("fideos")));
+	}
+}
