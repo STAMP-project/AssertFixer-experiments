783ab6b0bb7a2ba381ccc7c9045f9677add283e6
Luc Esape <luc.esape@gmail.com>
2018-06-01 02:23:43 +0200

    Bug commit from the following repository atomix/atomix This bug commit is a reflect of source code from: http://github.com/atomix/atomix/commit/9699ef159f4a25cd82a4c6203c0788f9c2de3baf.

---
+++ core/src/main/java/io/atomix/core/profile/DataGridProfile.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2018-present Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.atomix.core.profile;
+
+import io.atomix.core.AtomixConfig;
+import io.atomix.primitive.partition.MemberGroupStrategy;
+import io.atomix.protocols.backup.partition.PrimaryBackupPartitionGroupConfig;
+
+import static io.atomix.core.profile.AbstractProfile.findProfileType;
+
+/**
+ * In-memory data grid profile.
+ */
+public class DataGridProfile implements Profile {
+
+  /**
+   * The data-grid profile type.
+   */
+  private static final ProfileType TYPE = findProfileType(DataGridProfile.class);
+
+  private static final String SYSTEM_GROUP_NAME = "system";
+  private static final String GROUP_NAME = "data";
+  private static final int NUM_PARTITIONS = 71;
+
+  @Override
+  public ProfileType type() {
+    return TYPE;
+  }
+
+  @Override
+  public void configure(AtomixConfig config) {
+    if (config.getManagementGroup() == null) {
+      config.setManagementGroup(new PrimaryBackupPartitionGroupConfig()
+          .setName(SYSTEM_GROUP_NAME)
+          .setPartitions(1)
+          .setMemberGroupStrategy(MemberGroupStrategy.RACK_AWARE));
+    }
+    config.addPartitionGroup(new PrimaryBackupPartitionGroupConfig()
+        .setName(GROUP_NAME)
+        .setPartitions(NUM_PARTITIONS)
+        .setMemberGroupStrategy(MemberGroupStrategy.RACK_AWARE));
+  }
+}
