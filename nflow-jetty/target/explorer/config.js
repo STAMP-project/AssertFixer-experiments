e1be88400f51dd6d8aa69b95d999d93b17e79822
Luc Esape <luc.esape@gmail.com>
2018-06-05 11:55:24 +0200

    Bug commit from the following repository NitorCreations/nflow This bug commit is a reflect of source code from: http://github.com/NitorCreations/nflow/commit/beae625828f6bde5d1fed2d0cff6eac3b7f0940e.

---
+++ nflow-jetty/target/explorer/config.js
@@ -0,0 +1,28 @@
+'use strict';
+var Config = function() {
+  // these get overwritten in EndpointService
+  this.nflowUrl = 'http://localhost:7500/api/nflow';
+  this.nflowApiDocs = 'http://localhost:7500/doc/';
+
+  this.nflowEndpoints = [
+    {
+      id: 'localhost',
+      title: 'local nflow instance',
+      apiUrl: 'http://localhost:7500/api/nflow',
+      docUrl: 'http://localhost:7500/doc/'
+    },
+    {
+      id: 'nbank',
+      title: 'nBank at nflow.io',
+      apiUrl: 'http://bank.nflow.io/nflow/api/nflow',
+      docUrl: 'http://bank.nflow.io/nflow/doc/'
+    },
+  ];
+
+  this.radiator = {
+    // poll period in seconds
+    pollPeriod: 15,
+    // max number of items to keep in memory
+    maxHistorySize: 10000
+  };
+};
