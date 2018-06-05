3aa8b9d4fe57be3ea9d375845c116957af703e01
Luc Esape <luc.esape@gmail.com>
2018-06-05 13:49:49 +0200

    Bug commit from the following repository Ericsson/eiffel-intelligence This bug commit is a reflect of source code from: http://github.com/Ericsson/eiffel-intelligence/commit/cad71692d01644c52e4d444f0c115c981a3a9f23.

---
+++ target/cucumber-reports/TestSubscriptionCRUDRunner/report.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/functionaltests/resources/features/subscriptionCRUD.feature");
+formatter.feature({
+  "comments": [
+    {
+      "line": 1,
+      "value": "#Author: vasile.baluta@ericsson.com"
+    },
+    {
+      "line": 2,
+      "value": "#Keywords Summary :"
+    },
+    {
+      "line": 3,
+      "value": "#Feature: List of scenarios."
+    },
+    {
+      "line": 4,
+      "value": "#Scenario: Business rule through list of steps with arguments."
+    },
+    {
+      "line": 5,
+      "value": "#Given: Some precondition step"
+    },
+    {
+      "line": 6,
+      "value": "#When: Some key actions"
+    },
+    {
+      "line": 7,
+      "value": "#Then: To observe outcomes or validation"
+    },
+    {
+      "line": 8,
+      "value": "#And,But: To enumerate more Given,When,Then steps"
+    },
+    {
+      "line": 9,
+      "value": "#Scenario Outline: List of steps for data-driven as an Examples and \u003cplaceholder\u003e"
+    },
+    {
+      "line": 10,
+      "value": "#Examples: Container for s table"
+    },
+    {
+      "line": 11,
+      "value": "#Background: List of steps run before each of the scenarios"
+    },
+    {
+      "line": 12,
+      "value": "#\"\"\" (Doc Strings)"
+    },
+    {
+      "line": 13,
+      "value": "#| (Data Tables)"
+    },
+    {
+      "line": 14,
+      "value": "#@ (Tags/Labels):To group Scenarios"
+    },
+    {
+      "line": 15,
+      "value": "#\u003c\u003e (placeholder)"
+    },
+    {
+      "line": 16,
+      "value": "#\"\""
+    },
+    {
+      "line": 17,
+      "value": "## (Comments)"
+    },
+    {
+      "line": 18,
+      "value": "#Sample Feature Definition Template"
+    }
+  ],
+  "line": 20,
+  "name": "Test Subscription CRUD",
+  "description": "",
+  "id": "test-subscription-crud",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 19,
+      "name": "@tag"
+    }
+  ]
+});
+});
