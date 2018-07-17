93e756d26f014ff3b704624170e2702c505ef4fe
Luc Esape <luc.esape@gmail.com>
2018-07-17 09:25:15 +0200

    Bug commit from the following repository molgenis/molgenis This bug commit is a reflect of source code from: http://github.com/molgenis/molgenis/commit/f23d9481dc9642899083db925af13181b5c68922.

---
+++ molgenis-security-core/src/main/java/org/molgenis/security/core/runas/SystemSecurityToken.java
@@ -0,0 +1,30 @@
+package org.molgenis.security.core.runas;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Arrays;
+
+import static java.util.Collections.singletonList;
+import static org.molgenis.security.core.utils.SecurityUtils.*;
+
+/**
+ * Authentication token for the SYSTEM user
+ */
+public class SystemSecurityToken extends UsernamePasswordAuthenticationToken
+{
+	private static final long serialVersionUID = 2019504169566855264L;
+
+	public static final String ROLE_SYSTEM = "ROLE_SYSTEM";
+	public static final String USER_SYSTEM = "SYSTEM";
+
+	public SystemSecurityToken()
+	{
+		super(new User(USER_SYSTEM, "", singletonList(new SimpleGrantedAuthority(ROLE_SYSTEM))), "",
+				Arrays.asList(new SimpleGrantedAuthority(ROLE_SYSTEM),
+						new SimpleGrantedAuthority(ROLE_ACL_TAKE_OWNERSHIP),
+						new SimpleGrantedAuthority(ROLE_ACL_MODIFY_AUDITING),
+						new SimpleGrantedAuthority(ROLE_ACL_GENERAL_CHANGES)));
+	}
+}
