e8082cb0b51bb66c9dbde25715f1d0c3dc347705
Luc Esape <luc.esape@gmail.com>
2018-07-23 18:29:22 +0200

    Bug commit from the following repository DieguinhoHR/ponto-inteligente-api This bug commit is a reflect of source code from: http://github.com/DieguinhoHR/ponto-inteligente-api/commit/9363b3bdfcd5306b4b1fc7b573d6c28e9c706896.

---
+++ src/main/java/com/agilworks/pontoingeligente/PontoIngeligenteApplication.java
@@ -0,0 +1,12 @@
+package com.agilworks.pontoingeligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoIngeligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoIngeligenteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/agilworks/pontoingeligente/PontoIngeligenteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.agilworks.pontoingeligente;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PontoIngeligenteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
