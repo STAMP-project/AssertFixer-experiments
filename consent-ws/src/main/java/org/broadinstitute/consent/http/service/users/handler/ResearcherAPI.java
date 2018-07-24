64426c9e71d91c1f309c81aa16eb45edd39ac9ea
Luc Esape <luc.esape@gmail.com>
2018-07-24 19:43:00 +0200

    Bug commit from the following repository DataBiosphere/consent This bug commit is a reflect of source code from: http://github.com/DataBiosphere/consent/commit/6b612a509b78f8e2b9ba8a10cb9b774bd878c519.

---
+++ consent-ws/src/main/java/org/broadinstitute/consent/http/service/users/handler/ResearcherAPI.java
@@ -0,0 +1,26 @@
+package org.broadinstitute.consent.http.service.users.handler;
+import com.google.inject.ImplementedBy;
+import org.broadinstitute.consent.http.models.ResearcherProperty;
+
+import javax.ws.rs.NotFoundException;
+import java.util.List;
+import java.util.Map;
+
+
+@ImplementedBy(DatabaseResearcherAPI.class)
+public interface ResearcherAPI {
+
+    List<ResearcherProperty> registerResearcher(Map<String, String> researcherProperties, Integer userId, Boolean validate) throws NotFoundException, IllegalArgumentException, UnsupportedOperationException;
+
+    List<ResearcherProperty> updateResearcher(Map<String, String> researcherProperties, Integer userId, Boolean validate) throws NotFoundException, IllegalArgumentException;
+
+    Map<String, String> describeResearcherPropertiesMap(Integer userId) throws NotFoundException;
+
+    String insertEraByResearcherId(Integer userId, String token);
+
+    void deleteResearcherProperties(Integer userId);
+
+    Map<String, String> describeResearcherPropertiesForDAR(Integer userId);
+
+    List<ResearcherProperty> updateEraByResearcherId(Integer userId, String eraToken, Boolean eraStatus);
+}
