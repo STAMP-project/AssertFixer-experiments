26aec5fb03c1d184278d4a4ccb15265607ae0f8e
Luc Esape <luc.esape@gmail.com>
2018-07-27 04:36:28 +0200

    Bug commit from the following repository apache/nifi This bug commit is a reflect of source code from: http://github.com/apache/nifi/commit/ad7265839c96c16c2f7503fa7f113e258a440bcc.

---
+++ nifi-nar-bundles/nifi-gcp-bundle/nifi-gcp-processors/src/test/java/org/apache/nifi/processors/gcp/bigquery/AbstractBigQueryIT.java
@@ -0,0 +1,68 @@
+package org.apache.nifi.processors.gcp.bigquery;
+
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.bigquery.Dataset;
+import com.google.cloud.bigquery.DatasetInfo;
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.processors.gcp.credentials.service.GCPCredentialsControllerService;
+import org.apache.nifi.reporting.InitializationException;
+import org.apache.nifi.util.TestRunner;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.junit.Assert.assertNotNull;
+
+public abstract class AbstractBigQueryIT {
+
+    private static final String SERVICE_ACCOUNT_JSON_FILE = System.getProperty("user.home") + "/gcp-credentials.json";
+    static final String CONTROLLER_SERVICE = "GCPCredentialsService";
+    static final String PUT_LARGE_PAYLOAD_TEST_TABLE_NAME = "put_large_payload_test";
+    protected static BigQuery bigquery;
+    protected static Dataset dataset;
+    protected static TestRunner runner;
+
+    @BeforeClass
+    public static void beforeClass() throws IOException {
+        dataset = null;
+        BigQueryOptions bigQueryOptions = BigQueryOptions.newBuilder()
+                .build();
+        bigquery = bigQueryOptions.getService();
+
+        // Prepare a new data set
+        String datasetName = UUID.randomUUID().toString().replaceAll("-", "");
+        DatasetInfo datasetInfo = DatasetInfo.newBuilder("1test0" + datasetName).build();
+        dataset = bigquery.create(datasetInfo);
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        bigquery.delete(dataset.getDatasetId(), BigQuery.DatasetDeleteOption.deleteContents());
+    }
+
+    protected static void validateServiceExceptionAttribute(FlowFile flowFile) {
+        assertNotNull(flowFile.getAttribute(BigQueryAttributes.JOB_ERROR_MSG_ATTR));
+        assertNotNull(flowFile.getAttribute(BigQueryAttributes.JOB_ERROR_REASON_ATTR));
+        assertNotNull(flowFile.getAttribute(BigQueryAttributes.JOB_ERROR_LOCATION_ATTR));
+    }
+
+    protected TestRunner setCredentialsControllerService(TestRunner runner) throws InitializationException {
+        final Map<String, String> propertiesMap = new HashMap<>();
+        final GCPCredentialsControllerService credentialsControllerService = new GCPCredentialsControllerService();
+
+        propertiesMap.put("application-default-credentials", "false");
+        propertiesMap.put("compute-engine-credentials", "false");
+        propertiesMap.put("service-account-json-file", SERVICE_ACCOUNT_JSON_FILE);
+
+        runner.addControllerService(CONTROLLER_SERVICE, credentialsControllerService, propertiesMap);
+        runner.enableControllerService(credentialsControllerService);
+        runner.assertValid(credentialsControllerService);
+
+        return runner;
+    }
+}
---
+++ nifi-nar-bundles/nifi-gcp-bundle/nifi-gcp-processors/src/test/java/org/apache/nifi/processors/gcp/bigquery/PutGetDeleteGetBigQueryBatchIT.java
@@ -0,0 +1,73 @@
+package org.apache.nifi.processors.gcp.bigquery;
+
+import com.google.cloud.bigquery.FormatOptions;
+import org.apache.nifi.processors.gcp.AbstractGCPProcessor;
+import org.apache.nifi.reporting.InitializationException;
+import org.apache.nifi.util.TestRunners;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PutGetDeleteGetBigQueryBatchIT extends AbstractBigQueryIT {
+
+    @BeforeClass
+    public static void setup() {
+        runner = TestRunners.newTestRunner(PutBigQueryBatch.class);
+    }
+
+    @Test
+    public void largePayloadTest() throws InitializationException, IOException {
+        runner = setCredentialsControllerService(runner);
+        runner.setProperty(AbstractGCPProcessor.GCP_CREDENTIALS_PROVIDER_SERVICE, CONTROLLER_SERVICE);
+        runner.setProperty(BigQueryAttributes.DATASET_ATTR, dataset.getDatasetId().getDataset());
+        runner.setProperty(BigQueryAttributes.TABLE_NAME_ATTR, PUT_LARGE_PAYLOAD_TEST_TABLE_NAME);
+        runner.setProperty(BigQueryAttributes.SOURCE_TYPE_ATTR, FormatOptions.json().getType());
+        String schema = "[\n" +
+                "  {\n" +
+                "    \"description\": \"name\",\n" +
+                "    \"mode\": \"NULLABLE\",\n" +
+                "    \"name\": \"name\",\n" +
+                "    \"type\": \"STRING\"\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"description\": \"field 2\",\n" +
+                "    \"mode\": \"NULLABLE\",\n" +
+                "    \"name\": \"field_2\",\n" +
+                "    \"type\": \"STRING\"\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"description\": \"field 3\",\n" +
+                "    \"mode\": \"NULLABLE\",\n" +
+                "    \"name\": \"field_3\",\n" +
+                "    \"type\": \"STRING\"\n" +
+                "  }\n" +
+                "]";
+        runner.setProperty(BigQueryAttributes.TABLE_SCHEMA_ATTR, schema);
+        String str = "{\"name\":\"Bender Rodriguez\",\"field_2\":\"Daniel is great\",\"field_3\":\"“Here's to the crazy ones. The misfits.The rebels. The troublemakers." +
+                " The round pegs in the square holes. The ones who see things differently.They're not fond of rules. And they have no respect" +
+                " for the status quo. You can quote them, disagree with them,glorify or vilify them. About the only thing you can't do is ignore" +
+                " them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius." +
+                " Because the people who are crazy enough to think they can change the world, are the ones who do.” –Rob Siltanen\"}\r\n";
+        Path path = Paths.get(System.getProperty("java.io.tmpdir") + "/test");
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+
+            for (int i = 0; i < 5; i++) {
+                for (int ii = 0; ii < 1_000_000; ii++) {
+                    writer.write(str);
+                }
+                writer.flush();
+            }
+            writer.flush();
+        }
+
+        runner.enqueue(Files.newInputStream(path));
+        runner.run(1);
+        runner.assertAllFlowFilesTransferred(AbstractBigQueryProcessor.REL_SUCCESS, 1);
+    }
+}
