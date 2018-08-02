cd6db020fcd304582028256b07575aa48aaed6bd
Luc Esape <luc.esape@gmail.com>
2018-08-02 14:01:50 +0200

    Bug commit from the following repository MPI-ExperimentGroup/ExperimentTemplate This bug commit is a reflect of source code from: http://github.com/MPI-ExperimentGroup/ExperimentTemplate/commit/7e4ec35ff729c0aa49843b92e8fc7339baf6ba8f.

---
+++ registration/src/main/java/nl/mpi/tg/eg/frinex/rest/TagPairRepository3.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2018 Max Planck Institute for Psycholinguistics, Nijmegen
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package nl.mpi.tg.eg.frinex.rest;
+
+import java.util.List;
+import nl.mpi.tg.eg.frinex.model.TagPairData;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+/**
+ * @since Aug 2, 2018 11:19:36 AM (creation date)
+ * @author Peter Withers <peter.withers@mpi.nl>
+ */
+@RepositoryRestResource(collectionResourceRel = "tagpair3", path = "tagpair3")
+public interface TagPairRepository3 extends PagingAndSortingRepository<TagPairData, Long> {
+
+    List<TagPairData> findByUserId(@Param("userId") String userId);
+
+    List<TagPairData> findByEventTag(@Param("eventTag") String eventTag);
+
+//    List<TagPairData> findAllDistinctRecords();
+
+    List<TagPairData> findByUserIdOrderByTagDateAsc(@Param("userId") String userId);
+
+    List<TagPairData> findByUserIdAndEventTagOrderByTagDateAsc(@Param("userId") String userId, @Param("eventTag") String eventTag);
+}
