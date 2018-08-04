f551c04c848420873b4006db0a0020946493a259
Luc Esape <luc.esape@gmail.com>
2018-08-04 02:22:17 +0200

    Bug commit from the following repository MAPLE-Robot-Subgoaling/MAPLE-PALM This bug commit is a reflect of source code from: http://github.com/MAPLE-Robot-Subgoaling/MAPLE-PALM/commit/4930d72a7087dcbeecc574d8e8b2dd2f536fb801.

---
+++ src/main/java/edu/umbc/cs/maple/liftCopter/hierarchies/functions/GetFailurePF.java
@@ -0,0 +1,28 @@
+package edu.umbc.cs.maple.liftCopter.hierarchies.functions;
+
+import burlap.mdp.core.oo.propositional.PropositionalFunction;
+import burlap.mdp.core.oo.state.OOState;
+import burlap.mdp.core.oo.state.ObjectInstance;
+import edu.umbc.cs.maple.liftCopter.hierarchies.expert.tasks.get.state.LCGetState;
+import edu.umbc.cs.maple.liftCopter.state.LiftCopterState;
+
+import java.util.List;
+
+import static edu.umbc.cs.maple.liftCopter.LiftCopterConstants.*;
+
+public class GetFailurePF extends PropositionalFunction {
+    //get fails if any passenger if in taxi unless it is the right one
+
+    public GetFailurePF() {
+        super("getFail", new String[]{CLASS_CARGO});
+    }
+
+    @Override
+    public boolean isTrue(OOState s, String... params) {
+        if(((LCGetState)s).getAgentAtt(ATT_LOCATION) == ATT_VAL_CRASHED){
+            System.out.println("Get Failed");
+        }
+        return (((LCGetState)s).getAgentAtt(ATT_LOCATION) == ATT_VAL_CRASHED);
+    }
+
+}
