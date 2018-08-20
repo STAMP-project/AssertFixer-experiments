ef290d933c3aa86b55706d3903f5dfa3229684ab
Luc Esape <luc.esape@gmail.com>
2018-08-20 21:34:22 +0200

    Bug commit from the following repository Texera/texera This bug commit is a reflect of source code from: http://github.com/Texera/texera/commit/58c04824e3108b854c49b516c4cd478ec1f730b4.

---
+++ core/new-gui/src/app/workspace/component/product-tour/product-tour.component.spec.ts
@@ -0,0 +1,27 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductTourComponent } from './product-tour.component';
+
+import { TourService, IStepOption } from 'ngx-tour-ng-bootstrap';
+
+describe('ProductTourComponent', () => {
+  let component: ProductTourComponent;
+  let fixture: ComponentFixture<ProductTourComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProductTourComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductTourComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
