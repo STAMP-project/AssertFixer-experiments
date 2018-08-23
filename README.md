64a2d89f723ea1664832ab8919b55c7960f118bc
Luc Esape <luc.esape@gmail.com>
2018-08-23 05:20:00 +0200

    Bug commit from the following repository victorbrugnolo/ponto-inteligente-java-api This bug commit is a reflect of source code from: http://github.com/victorbrugnolo/ponto-inteligente-java-api/commit/fe1e2ec0e8c9e54a06bba43bb98af21f75214fcb.

---
+++ README.md
@@ -0,0 +1,4 @@
+[![Build Status](https://travis-ci.org/victorbrugnolo/ponto-inteligente-java-api.svg?branch=master)](https://travis-ci.org/victorbrugnolo/ponto-inteligente-java-api)
+
+# Ponto Inteligente
+API de ponto inteligente em Java com Spring Boot.
---
+++ src/main/java/com/victorbrugnolo/pontointeligentejava/PontoInteligenteJavaApplication.java
@@ -0,0 +1,12 @@
+package com.victorbrugnolo.pontointeligentejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteJavaApplication.class, args);
+	}
+}
