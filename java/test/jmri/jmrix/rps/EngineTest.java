f90aaf30311fb857f051b9a3a149c979702d47a5
Luc Esape <luc.esape@gmail.com>
2018-09-02 20:13:21 +0200

    Bug commit from the following repository JMRI/JMRI This bug commit is a reflect of source code from: http://github.com/JMRI/JMRI/commit/9bd1138975f9e2482808a38b432df481e053708b.

---
+++ java/test/jmri/jmrix/rps/EngineTest.java
@@ -0,0 +1,53 @@
+package jmri.jmrix.rps;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.Assert;
+
+/**
+ * JUnit tests for the rps.Engine class.
+ *
+ * @author	Bob Jacobsen Copyright 2008
+  */
+public class EngineTest extends TestCase {
+
+    public void testCtor() {
+        Engine e = new Engine();
+        Assert.assertNotNull("exists", e);
+    }
+
+    public void testNumReceivers() {
+        Engine e = new Engine();
+        e.setMaxReceiverNumber(3);
+        Assert.assertEquals("number", 3, e.getMaxReceiverNumber());
+    }
+
+    // from here down is testing infrastructure
+    public EngineTest(String s) {
+        super(s);
+    }
+
+    // Main entry point
+    static public void main(String[] args) {
+        String[] testCaseName = {EngineTest.class.getName()};
+        junit.textui.TestRunner.main(testCaseName);
+    }
+
+    // test suite from all defined tests
+    public static Test suite() {
+        TestSuite suite = new TestSuite(EngineTest.class);
+        return suite;
+    }
+
+   @Override
+   public void setUp() {
+        jmri.util.JUnitUtil.setUp();
+   }
+
+   @Override
+   public void tearDown(){
+        jmri.util.JUnitUtil.tearDown();
+   }
+
+}
