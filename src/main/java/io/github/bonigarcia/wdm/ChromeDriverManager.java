284a60ec7b67744d814dc48cea454bd7de6f1b62
Luc Esape <luc.esape@gmail.com>
2018-09-02 15:48:36 +0200

    Bug commit from the following repository bonigarcia/webdrivermanager This bug commit is a reflect of source code from: http://github.com/bonigarcia/webdrivermanager/commit/dcb4cb791caa02d4eb6d9291e94a04ff9a039406.

---
+++ src/main/java/io/github/bonigarcia/wdm/ChromeDriverManager.java
@@ -0,0 +1,98 @@
+/*
+ * (C) Copyright 2015 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.wdm;
+
+import static io.github.bonigarcia.wdm.Config.isNullOrEmpty;
+import static io.github.bonigarcia.wdm.DriverManagerType.CHROME;
+import static io.github.bonigarcia.wdm.Shell.getProgramFilePath;
+import static io.github.bonigarcia.wdm.Shell.getVersionFromPosixOutput;
+import static io.github.bonigarcia.wdm.Shell.getVersionFromWmicOutput;
+import static io.github.bonigarcia.wdm.Shell.runAndWait;
+import static java.util.Arrays.asList;
+import static java.util.Optional.empty;
+import static org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Manager for Chrome.
+ *
+ * @author Boni Garcia (boni.gg@gmail.com)
+ * @since 1.0.0
+ */
+public class ChromeDriverManager extends WebDriverManager {
+
+    protected ChromeDriverManager() {
+        driverManagerType = CHROME;
+        exportParameterKey = "wdm.chromeDriverExport";
+        driverVersionKey = "wdm.chromeDriverVersion";
+        driverUrlKey = "wdm.chromeDriverUrl";
+        driverMirrorUrlKey = "wdm.chromeDriverMirrorUrl";
+        driverName = asList("chromedriver");
+    }
+
+    @Override
+    protected List<URL> getDrivers() throws IOException {
+        URL driverUrl = config().getDriverUrl(driverUrlKey);
+        List<URL> urls;
+        if (isUsingTaobaoMirror()) {
+            urls = getDriversFromMirror(driverUrl);
+        } else {
+            urls = getDriversFromXml(driverUrl);
+        }
+        return urls;
+    }
+
+    @Override
+    protected String getCurrentVersion(URL url, String driverName) {
+        if (isUsingTaobaoMirror()) {
+            int i = url.getFile().lastIndexOf(SLASH);
+            int j = url.getFile().substring(0, i).lastIndexOf(SLASH) + 1;
+            return url.getFile().substring(j, i);
+        } else {
+            return super.getCurrentVersion(url, driverName);
+        }
+    }
+
+    @Override
+    protected Optional<String> getBrowserVersion() {
+        if (IS_OS_WINDOWS) {
+            String programFiles = getProgramFilePath();
+            String browserVersionOutput = runAndWait("wmic", "datafile",
+                    "where",
+                    "name='" + programFiles
+                            + "\\\\Google\\\\Chrome\\\\Application\\\\chrome.exe'",
+                    "get", "Version", "/value");
+            if (!isNullOrEmpty(browserVersionOutput)) {
+                return Optional
+                        .of(getVersionFromWmicOutput(browserVersionOutput));
+            }
+        } else {
+            String browserVersionOutput = runAndWait("google-chrome",
+                    "--version");
+            if (!isNullOrEmpty(browserVersionOutput)) {
+                return Optional.of(getVersionFromPosixOutput(browserVersionOutput,
+                        driverManagerType));
+            }
+        }
+        return empty();
+    }
+
+}
---
+++ src/main/java/io/github/bonigarcia/wdm/Shell.java
@@ -0,0 +1,89 @@
+/*
+ * (C) Copyright 2018 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.wdm;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+
+/**
+ * Command line executor.
+ *
+ * @author Boni Garcia (boni.gg@gmail.com)
+ * @since 3.0.0
+ */
+public class Shell {
+
+    static final Logger log = getLogger(lookup().lookupClass());
+
+    private Shell() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static String runAndWait(String... command) {
+        return runAndWaitArray(command);
+    }
+
+    public static String runAndWaitArray(String[] command) {
+        String commandStr = Arrays.toString(command);
+        log.trace("Running command on the shell: {}", commandStr);
+        String result = runAndWaitNoLog(command);
+        log.trace("Result: {}", result);
+        return result;
+    }
+
+    public static String runAndWaitNoLog(String... command) {
+        String output = "";
+        try {
+            Process process = new ProcessBuilder(command)
+                    .redirectErrorStream(true).start();
+            output = IOUtils.toString(process.getInputStream(), UTF_8);
+            process.destroy();
+        } catch (IOException e) {
+            log.warn("There was an error executin commannd {} on the shell",
+                    command, e);
+        }
+        return output.trim();
+    }
+
+    public static String getVersionFromWmicOutput(String wmicOutput) {
+        int i = wmicOutput.indexOf('=');
+        int j = wmicOutput.indexOf('.');
+        return i != -1 && j != -1 ? wmicOutput.substring(i + 1, j) : wmicOutput;
+    }
+
+    public static String getVersionFromPosixOutput(String posixOutput,
+            DriverManagerType driverManagerType) {
+        String driverType = driverManagerType.toString();
+        int i = posixOutput.indexOf(driverType);
+        int j = posixOutput.indexOf('.');
+        return i != -1 && j != -1
+                ? posixOutput.substring(i + driverType.length() + 1, j)
+                : posixOutput;
+    }
+
+    public static String getProgramFilePath() {
+        return System.getenv("PROGRAMFILES(X86)").replaceAll("\\\\",
+                "\\\\\\\\");
+    }
+}
