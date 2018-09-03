6fb8c27e8ea4b3cf9c345042d3b346be580b4640
Luc Esape <luc.esape@gmail.com>
2018-09-03 16:59:00 +0200

    Bug commit from the following repository spring-projects/spring-data-examples This bug commit is a reflect of source code from: http://github.com/spring-projects/spring-data-examples/commit/020b08dc8eab20098ec88004b6de67f6017cc90f.

---
+++ jpa/multiple-datasources/src/main/java/example/springdata/jpa/multipleds/customer/CustomerConfig.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2015-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.springdata.jpa.multipleds.customer;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+/**
+ * Configuration for the {@link Customer} slice of the system. A dedicated {@link DataSource},
+ * {@link JpaTransactionManager} and {@link EntityManagerFactory}. Note that there could of course be some
+ * de-duplication with {@link example.springdata.jpa.multipleds.order.OrderConfig}. I just decided to keep it to focus
+ * on the separation of the two. Also, some overlaps might not even occur in real world scenarios (whether to create DDl
+ * or the like).
+ *
+ * @author Oliver Gierke
+ */
+@Configuration
+@EnableJpaRepositories( //
+		entityManagerFactoryRef = "customerEntityManagerFactory", //
+		transactionManagerRef = "customerTransactionManager")
+class CustomerConfig {
+
+	@Bean
+	PlatformTransactionManager customerTransactionManager(
+			@Qualifier("customerEntityManagerFactory") EntityManagerFactory emf) {
+		return new JpaTransactionManager(emf);
+	}
+
+	@Bean
+	LocalContainerEntityManagerFactoryBean customerEntityManagerFactory(EntityManagerFactoryBuilder builder) {
+
+		return builder //
+				.dataSource(customerDataSource()) //
+				.packages(CustomerConfig.class) //
+				.build();
+	}
+
+	@Bean
+	DataSource customerDataSource() {
+
+		return new EmbeddedDatabaseBuilder() //
+				.setType(EmbeddedDatabaseType.HSQL) //
+				.setName("customers") //
+				.build();
+	}
+}
