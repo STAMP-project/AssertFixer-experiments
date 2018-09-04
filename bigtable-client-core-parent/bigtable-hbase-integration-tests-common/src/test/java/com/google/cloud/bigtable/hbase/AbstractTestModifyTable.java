e2c4ffd891a83b371828eb386b47d019be9c0617
Luc Esape <luc.esape@gmail.com>
2018-09-04 18:49:32 +0200

    Bug commit from the following repository GoogleCloudPlatform/cloud-bigtable-client This bug commit is a reflect of source code from: http://github.com/GoogleCloudPlatform/cloud-bigtable-client/commit/ed628a9b12549f06d63e02151a052373a019776e.

---
+++ bigtable-client-core-parent/bigtable-hbase-integration-tests-common/src/test/java/com/google/cloud/bigtable/hbase/AbstractTestModifyTable.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigtable.hbase;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import com.google.common.base.Function;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public abstract class AbstractTestModifyTable extends AbstractTest {
+  
+  public static final byte[] COLUMN_FAMILY2 = Bytes.toBytes("test_family2");
+
+  public void testModifyTable(Function<HTableDescriptor, Void> modifyTable) throws IOException {
+    TableName tableName = sharedTestEnv.newTestTableName();
+    sharedTestEnv.createTable(tableName);
+    try(Admin admin = getConnection().getAdmin()) {
+      HTableDescriptor descriptor = new HTableDescriptor(tableName);
+      descriptor.addFamily(new HColumnDescriptor(COLUMN_FAMILY2));
+      modifyTable.apply(descriptor);
+      assertTrue(admin.tableExists(tableName));
+      assertTrue(admin.getTableDescriptor(tableName).hasFamily(COLUMN_FAMILY2));
+      assertEquals(1, admin.getTableDescriptor(tableName).getColumnFamilies().length);
+      admin.disableTable(tableName);
+      admin.deleteTable(tableName);
+    }
+  }
+}
