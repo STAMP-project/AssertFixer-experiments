8d311758f223f2d20c1e8a39f03c20fd53bd2d74
Luc Esape <luc.esape@gmail.com>
2018-09-06 12:02:14 +0200

    Bug commit from the following repository rootrock13/topjava This bug commit is a reflect of source code from: http://github.com/rootrock13/topjava/commit/40e1e72e11dc8f6ca85d50b78e2a7637ec15c3bc.

---
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -0,0 +1,28 @@
+package ru.javawebinar.topjava.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:meals";
+    }
+
+    @GetMapping("/users")
+    public String users() {
+        return "users";
+    }
+
+    @GetMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/meals")
+    public String meals() {
+        return "meals";
+    }
+}
