e05f83aaf5e2a2c508b96954bc6b38e6bd7f5ebc
Luc Esape <luc.esape@gmail.com>
2018-09-07 16:58:13 +0200

    Bug commit from the following repository resteasy/Resteasy This bug commit is a reflect of source code from: http://github.com/resteasy/Resteasy/commit/3ad9d2cf1ddb8bc7e43c1ee136630cd234137d31.

---
+++ resteasy-jaxrs/src/main/java/org/jboss/resteasy/spi/ResteasyAsynchronousContext.java
@@ -0,0 +1,19 @@
+package org.jboss.resteasy.spi;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
+ * @version $Revision: 1 $
+ */
+public interface ResteasyAsynchronousContext
+{
+   boolean isSuspended();
+   ResteasyAsynchronousResponse getAsyncResponse();
+
+   ResteasyAsynchronousResponse suspend() throws IllegalStateException;
+
+   ResteasyAsynchronousResponse suspend(long millis) throws IllegalStateException;
+
+   ResteasyAsynchronousResponse suspend(long time, TimeUnit unit) throws IllegalStateException;
+}
