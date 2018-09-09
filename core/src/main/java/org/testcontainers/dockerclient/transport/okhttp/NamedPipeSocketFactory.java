a6f0b961f7eb368437d5fe67aaf64d2946621eae
Luc Esape <luc.esape@gmail.com>
2018-09-09 22:58:48 +0200

    Bug commit from the following repository testcontainers/testcontainers-java This bug commit is a reflect of source code from: http://github.com/testcontainers/testcontainers-java/commit/b81970ff014d88346d6c981d1d61de267743beb3.

---
+++ core/src/main/java/org/testcontainers/dockerclient/transport/okhttp/NamedPipeSocketFactory.java
@@ -0,0 +1,58 @@
+package org.testcontainers.dockerclient.transport.okhttp;
+
+import de.gesellix.docker.client.filesocket.HostnameEncoder;
+import de.gesellix.docker.client.filesocket.NamedPipeSocket;
+import lombok.EqualsAndHashCode;
+import lombok.SneakyThrows;
+import lombok.Value;
+
+import javax.net.SocketFactory;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+public class NamedPipeSocketFactory extends SocketFactory {
+
+    String socketPath;
+
+    @Override
+    @SneakyThrows
+    public Socket createSocket() {
+        return new NamedPipeSocket() {
+            @Override
+            public void connect(SocketAddress endpoint, int timeout) throws IOException {
+                super.connect(
+                    new InetSocketAddress(
+                        new HostnameEncoder().encode(socketPath),
+                        0
+                    ),
+                    timeout
+                );
+            }
+        };
+    }
+
+    @Override
+    public Socket createSocket(String s, int i) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Socket createSocket(String s, int i, InetAddress inetAddress, int i1) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Socket createSocket(InetAddress inetAddress, int i) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Socket createSocket(InetAddress inetAddress, int i, InetAddress inetAddress1, int i1) {
+        throw new UnsupportedOperationException();
+    }
+}
