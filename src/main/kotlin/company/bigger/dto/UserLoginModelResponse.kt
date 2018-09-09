dacd5b852cc00847fe918d3cfb876408a4faeeb9
Luc Esape <luc.esape@gmail.com>
2018-09-09 07:55:35 +0200

    Bug commit from the following repository iDempiere-micro/idempiere-micro-spring This bug commit is a reflect of source code from: http://github.com/iDempiere-micro/idempiere-micro-spring/commit/3a19dd471114a33c60fcfd30e27ba8b943ffbd04.

---
+++ src/main/kotlin/company/bigger/dto/UserLoginModelResponse.kt
@@ -0,0 +1,44 @@
+package company.bigger.dto
+
+import software.hsharp.core.models.INameKeyPair
+import java.util.Arrays
+
+data class UserLoginModelResponse(
+        override val logged: Boolean = false,
+        val clients: Array<INameKeyPair> = arrayOf(),
+        val roles: Array<INameKeyPair> = arrayOf(),
+        val orgs: Array<INameKeyPair> = arrayOf(),
+        val warehouses: Array<INameKeyPair> = arrayOf(),
+        override val token: String? = null,
+        override val loginName: String,
+        override val clientId: Int = -1,
+        override val userId: Int = -1
+) : ILoginResponse {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as UserLoginModelResponse
+
+        if (logged != other.logged) return false
+        if (!Arrays.equals(clients, other.clients)) return false
+        if (!Arrays.equals(roles, other.roles)) return false
+        if (!Arrays.equals(orgs, other.orgs)) return false
+        if (!Arrays.equals(warehouses, other.warehouses)) return false
+        if (token != other.token) return false
+        if (loginName != other.loginName) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = logged.hashCode()
+        result = 31 * result + Arrays.hashCode(clients)
+        result = 31 * result + Arrays.hashCode(roles)
+        result = 31 * result + Arrays.hashCode(orgs)
+        result = 31 * result + Arrays.hashCode(warehouses)
+        result = 31 * result + (token?.hashCode() ?: 0)
+        result = 31 * result + (loginName.hashCode())
+        return result
+    }
+}
