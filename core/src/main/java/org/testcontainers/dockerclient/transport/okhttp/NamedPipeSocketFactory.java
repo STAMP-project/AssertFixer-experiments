917aa8ceb4c0dddbfab77757d73ff8ab82489c7c
Luc Esape <luc.esape@gmail.com>
2018-09-10 07:48:08 +0200

    Bug commit from the following repository testcontainers/testcontainers-java This bug commit is a reflect of source code from: http://github.com/testcontainers/testcontainers-java/commit/01b406e132706314c9b73ebf539c1a71df93c690.

---
+++ core/src/main/java/org/testcontainers/dockerclient/transport/okhttp/NamedPipeSocketFactory.java
@@ -0,0 +1,43 @@
+package org.testcontainers.dockerclient.transport.okhttp;
+
+import lombok.EqualsAndHashCode;
+import lombok.SneakyThrows;
+import lombok.Value;
+
+import javax.net.SocketFactory;
+import java.net.InetAddress;
+import java.net.Socket;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+public class NamedPipeSocketFactory extends SocketFactory {
+
+    String socketPath;
+
+    @Override
+    @SneakyThrows
+    public Socket createSocket() {
+        String pipeName = socketPath.substring("//./pipe/".length()).replace("/", "\\");
+        return new NamedPipeSocket("localhost", pipeName);
+    }
+
+    @Override
+    public Socket createSocket(String s, int i) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Socket createSocket(String s, int i, InetAddress inetAddress, int i1) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Socket createSocket(InetAddress inetAddress, int i) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Socket createSocket(InetAddress inetAddress, int i, InetAddress inetAddress1, int i1) {
+        throw new UnsupportedOperationException();
+    }
+}
