e7753b22e706cb6c71135d268d6b77669c103779
Luc Esape <luc.esape@gmail.com>
2018-09-11 08:01:25 +0200

    Bug commit from the following repository bobjacobsen/JMRI This bug commit is a reflect of source code from: http://github.com/bobjacobsen/JMRI/commit/e2f1163bf67cc5bbf2e56a087db5441623801dea.

---
+++ java/test/jmri/jmrix/dccpp/DCCppMultiMeterTest.java
@@ -0,0 +1,26 @@
+package jmri.jmrix.dccpp;
+
+import jmri.util.JUnitUtil;
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ *
+ * @author Paul Bender Copyright (C) 2017	
+ */
+public class DCCppMultiMeterTest extends jmri.implementation.AbstractMultiMeterTestBase{
+
+    @Override
+    @Before
+    public void setUp() {
+        JUnitUtil.setUp();
+        // infrastructure objects
+        DCCppInterfaceScaffold tc = new DCCppInterfaceScaffold(new DCCppCommandStation());
+
+        DCCppSystemConnectionMemo memo = new DCCppSystemConnectionMemo(tc);
+        mm = new DCCppMultiMeter(memo);
+    }
+
+    // private final static Logger log = LoggerFactory.getLogger(DCCppMultiMeterTest.class);
+
+}
