069cad1dde496b17a8e33e4e6becf3c8fe63fb81
Luc Esape <luc.esape@gmail.com>
2018-09-11 12:53:09 +0200

    Bug commit from the following repository scalecube/scalecube-gateway This bug commit is a reflect of source code from: http://github.com/scalecube/scalecube-gateway/commit/ada579987025ff7b21bc891111c25e796415c3d6.

---
+++ GatewayRunner/src/main/java/io/scalecube/gateway/GatewayRunner.java
@@ -0,0 +1,123 @@
+package io.scalecube.gateway;
+
+import com.codahale.metrics.CsvReporter;
+import com.codahale.metrics.MetricRegistry;
+import io.scalecube.config.ConfigRegistry;
+import io.scalecube.gateway.config.GatewayConfigRegistry;
+import io.scalecube.gateway.http.HttpGateway;
+import io.scalecube.gateway.rsocket.websocket.RSocketWebsocketGateway;
+import io.scalecube.gateway.websocket.WebsocketGateway;
+import io.scalecube.services.Microservices;
+import io.scalecube.services.gateway.GatewayConfig;
+import io.scalecube.transport.Address;
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GatewayRunner {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(GatewayRunner.class);
+  private static final String DECORATOR =
+      "#######################################################################";
+
+  private static final String REPORTER_PATH = "reports/gw/metrics";
+
+  /**
+   * Main runner.
+   *
+   * @param args program arguments
+   * @throws Exception exception thrown
+   */
+  public static void main(String[] args) throws Exception {
+    ConfigRegistry configRegistry = GatewayConfigRegistry.configRegistry();
+
+    Config config =
+        configRegistry
+            .objectProperty("io.scalecube.gateway", Config.class)
+            .value()
+            .orElseThrow(() -> new IllegalStateException("Couldn't load config"));
+
+    LOGGER.info(DECORATOR);
+    LOGGER.info("Starting Gateway on " + config);
+    LOGGER.info(DECORATOR);
+
+    int servicePort = config.getServicePort();
+    int discoveryPort = config.getDiscoveryPort();
+    Address[] seeds = config.getSeeds().stream().map(Address::from).toArray(Address[]::new);
+
+    MetricRegistry metrics = initMetricRegistry();
+
+    Microservices.builder()
+        .seeds(seeds)
+        .servicePort(servicePort)
+        .discoveryPort(discoveryPort)
+        .gateway(GatewayConfig.builder("ws", WebsocketGateway.class).port(7070).build())
+        .gateway(GatewayConfig.builder("http", HttpGateway.class).port(8080).build())
+        .gateway(GatewayConfig.builder("rsws", RSocketWebsocketGateway.class).port(9090).build())
+        .metrics(metrics)
+        .startAwait();
+
+    Thread.currentThread().join();
+  }
+
+  private static MetricRegistry initMetricRegistry() {
+    MetricRegistry metrics = new MetricRegistry();
+    File reporterDir = new File(REPORTER_PATH);
+    if (!reporterDir.exists()) {
+      //noinspection ResultOfMethodCallIgnored
+      reporterDir.mkdirs();
+    }
+    CsvReporter csvReporter =
+        CsvReporter.forRegistry(metrics)
+            .convertDurationsTo(TimeUnit.MILLISECONDS)
+            .convertRatesTo(TimeUnit.SECONDS)
+            .build(reporterDir);
+
+    csvReporter.start(10, TimeUnit.SECONDS);
+    return metrics;
+  }
+
+  public static class Config {
+
+    private int servicePort;
+    private int discoveryPort;
+    private List<String> seeds;
+
+    public Config() {}
+
+    public int getServicePort() {
+      return servicePort;
+    }
+
+    public void setServicePort(int servicePort) {
+      this.servicePort = servicePort;
+    }
+
+    public int getDiscoveryPort() {
+      return discoveryPort;
+    }
+
+    public void setDiscoveryPort(int discoveryPort) {
+      this.discoveryPort = discoveryPort;
+    }
+
+    public List<String> getSeeds() {
+      return seeds;
+    }
+
+    public void setSeeds(List<String> seeds) {
+      this.seeds = seeds;
+    }
+
+    @Override
+    public String toString() {
+      final StringBuilder sb = new StringBuilder("Config{");
+      sb.append("servicePort=").append(servicePort);
+      sb.append(", seeds='").append(seeds).append('\'');
+      sb.append('}');
+      return sb.toString();
+    }
+  }
+}
---
+++ Websocket/src/main/java/io/scalecube/gateway/websocket/GatewayWebsocketAcceptor.java
@@ -0,0 +1,217 @@
+package io.scalecube.gateway.websocket;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.util.ReferenceCountUtil;
+import io.scalecube.gateway.GatewayMetrics;
+import io.scalecube.gateway.websocket.message.GatewayMessage;
+import io.scalecube.gateway.websocket.message.GatewayMessageCodec;
+import io.scalecube.gateway.websocket.message.Signal;
+import io.scalecube.services.ServiceCall;
+import io.scalecube.services.api.ServiceMessage;
+import io.scalecube.services.exceptions.BadRequestException;
+import io.scalecube.services.exceptions.ExceptionProcessor;
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.BiFunction;
+import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.publisher.Mono;
+import reactor.ipc.netty.http.server.HttpServerRequest;
+import reactor.ipc.netty.http.server.HttpServerResponse;
+import reactor.ipc.netty.http.websocket.WebsocketInbound;
+import reactor.ipc.netty.http.websocket.WebsocketOutbound;
+
+public class GatewayWebsocketAcceptor
+    implements BiFunction<HttpServerRequest, HttpServerResponse, Publisher<Void>> {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(GatewayWebsocketAcceptor.class);
+
+  private final ServiceCall serviceCall;
+  private final GatewayMetrics metrics;
+  private final GatewayMessageCodec messageCodec = new GatewayMessageCodec();
+
+  /**
+   * Constructor for websocket acceptor.
+   *
+   * @param serviceCall service call
+   * @param metrics metrics instance
+   */
+  public GatewayWebsocketAcceptor(ServiceCall serviceCall, GatewayMetrics metrics) {
+    this.serviceCall = serviceCall;
+    this.metrics = metrics;
+  }
+
+  @Override
+  public Publisher<Void> apply(HttpServerRequest httpRequest, HttpServerResponse httpResponse) {
+    return httpResponse.sendWebsocket(
+        (WebsocketInbound inbound, WebsocketOutbound outbound) ->
+            onConnect(new WebsocketSession(httpRequest, inbound, outbound)));
+  }
+
+  private Mono<Void> onConnect(WebsocketSession session) {
+    LOGGER.info("Session connected: " + session);
+    metrics.incConnection();
+
+    Mono<Void> voidMono =
+        session.send(
+            session
+                .receive()
+                .doOnNext(input -> metrics.markRequest())
+                .flatMap(message -> handleMessage(session, message))
+                .flatMap(this::toByteBuf)
+                .doOnNext(input -> metrics.markResponse())
+                .doOnError(
+                    ex ->
+                        LOGGER.error(
+                            "Unhandled exception occurred: {}, " + "session: {} will be closed",
+                            ex,
+                            session,
+                            ex)));
+
+    session.onClose(
+        () -> {
+          LOGGER.info("Session disconnected: " + session);
+          metrics.decConnection();
+        });
+
+    return voidMono.then();
+  }
+
+  private Flux<GatewayMessage> handleMessage(WebsocketSession session, ByteBuf message) {
+    return Flux.create(
+        sink -> {
+          Long sid = null;
+          try {
+            GatewayMessage request = toMessage(message);
+            Long streamId = sid = request.streamId();
+
+            // check message contains sid
+            checkSidNotNull(streamId, session, request);
+
+            // check session contains sid for CANCEL operation
+            if (request.hasSignal(Signal.CANCEL)) {
+              handleCancelRequest(streamId, session, request, sink);
+              return;
+            }
+
+            // check session doesn't contain sid yet
+            checkSidNotRegisteredYet(streamId, session, request);
+
+            // check message contains qualifier
+            checkQualifierNotNull(session, request);
+
+            AtomicBoolean receivedErrorMessage = new AtomicBoolean(false);
+
+            Flux<ServiceMessage> serviceStream =
+                serviceCall.requestMany(GatewayMessage.toServiceMessage(request));
+
+            if (request.inactivity() != null) {
+              serviceStream = serviceStream.timeout(Duration.ofMillis(request.inactivity()));
+            }
+
+            Disposable disposable =
+                serviceStream
+                    .map(response -> prepareResponse(streamId, response, receivedErrorMessage))
+                    .concatWith(Flux.defer(() -> prepareCompletion(streamId, receivedErrorMessage)))
+                    .onErrorResume(t -> {
+                      ReferenceCountUtil.safeRelease(message);
+                      return Mono.just(toErrorMessage(t, streamId));
+                    })
+                    .doFinally(signalType -> session.dispose(streamId))
+                    .subscribe(sink::next, sink::error, sink::complete);
+
+            session.register(sid, disposable);
+          } catch (Throwable ex) {
+            ReferenceCountUtil.safeRelease(message);
+            sink.next(toErrorMessage(ex, sid));
+            sink.complete();
+          }
+        });
+  }
+
+  private Mono<GatewayMessage> prepareCompletion(
+      Long streamId, AtomicBoolean receivedErrorMessage) {
+    return receivedErrorMessage.get()
+        ? Mono.empty()
+        : Mono.just(GatewayMessage.builder().streamId(streamId).signal(Signal.COMPLETE).build());
+  }
+
+  private GatewayMessage prepareResponse(
+      Long streamId, ServiceMessage message, AtomicBoolean receivedErrorMessage) {
+    GatewayMessage.Builder response = GatewayMessage.from(message).streamId(streamId);
+    if (ExceptionProcessor.isError(message)) {
+      receivedErrorMessage.set(true);
+      response.signal(Signal.ERROR);
+    }
+    return response.build();
+  }
+
+  private void checkQualifierNotNull(WebsocketSession session, GatewayMessage request) {
+    if (request.qualifier() == null) {
+      LOGGER.error("Bad gateway request {} on session {}: qualifier is missing", request, session);
+      throw new BadRequestException("qualifier is missing");
+    }
+  }
+
+  private void checkSidNotRegisteredYet(
+      Long streamId, WebsocketSession session, GatewayMessage request) {
+    if (session.containsSid(streamId)) {
+      LOGGER.error(
+          "Bad gateway request {} on session {}: sid={} is already registered", request, session);
+      throw new BadRequestException("sid=" + streamId + " is already registered");
+    }
+  }
+
+  private void handleCancelRequest(
+      Long streamId,
+      WebsocketSession session,
+      GatewayMessage request,
+      FluxSink<GatewayMessage> sink) {
+
+    if (!session.dispose(streamId)) {
+      LOGGER.error("CANCEL gateway request {} failed in session {}", request, streamId, session);
+      throw new BadRequestException("Failed CANCEL request");
+    }
+
+    // send message with CANCEL signal
+    sink.next(cancelResponse(streamId));
+    sink.complete();
+  }
+
+  private void checkSidNotNull(Long streamId, WebsocketSession session, GatewayMessage request) {
+    if (streamId == null) {
+      LOGGER.error("Bad gateway request {} on session {}: sid is missing", request, session);
+      throw new BadRequestException("sid is missing");
+    }
+  }
+
+  private Mono<ByteBuf> toByteBuf(GatewayMessage message) {
+    try {
+      return Mono.just(messageCodec.encode(message));
+    } catch (Throwable ex) {
+      ReferenceCountUtil.safeRelease(message.data());
+      return Mono.empty();
+    }
+  }
+
+  private GatewayMessage toMessage(ByteBuf message) {
+    try {
+      return messageCodec.decode(message);
+    } catch (Throwable ex) {
+      throw new BadRequestException(ex.getMessage());
+    }
+  }
+
+  private GatewayMessage toErrorMessage(Throwable th, Long streamId) {
+    ServiceMessage serviceMessage = ExceptionProcessor.toMessage(th);
+    return GatewayMessage.from(serviceMessage).streamId(streamId).signal(Signal.ERROR).build();
+  }
+
+  private GatewayMessage cancelResponse(Long streamId) {
+    return GatewayMessage.builder().streamId(streamId).signal(Signal.CANCEL).build();
+  }
+}
