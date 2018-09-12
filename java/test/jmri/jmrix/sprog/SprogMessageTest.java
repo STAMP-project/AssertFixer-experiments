abc99cd8d3251c46892f38816294f6521601d3df
Luc Esape <luc.esape@gmail.com>
2018-09-12 06:51:02 +0200

    Bug commit from the following repository JMRI/JMRI This bug commit is a reflect of source code from: http://github.com/JMRI/JMRI/commit/315450833f33180e34cef6c09aea9feb32e7b1e0.

---
+++ java/test/jmri/jmrix/sprog/SprogMessageTest.java
@@ -0,0 +1,56 @@
+package jmri.jmrix.sprog;
+
+import jmri.ProgrammingMode;
+import jmri.util.JUnitUtil;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * JUnit tests for the SprogMessage class.
+ *
+ * @author	Bob Jacobsen Copyright 2012
+ */
+public class SprogMessageTest extends jmri.jmrix.AbstractMessageTestBase {
+
+    private SprogMessage msg = null;
+
+    @Test
+    public void testReadCv() {
+        msg = SprogMessage.getReadCV(12, ProgrammingMode.PAGEMODE);
+        Assert.assertEquals("string compare ", "V 0012", msg.toString());
+    }
+
+    @Test
+    public void testWriteCV() {
+        msg = SprogMessage.getWriteCV(12, 251, ProgrammingMode.PAGEMODE);
+        Assert.assertEquals("string compare ", "V 0012 251", msg.toString());
+    }
+
+    @Test
+    public void testReadCvLarge() {
+        msg = SprogMessage.getReadCV(1021, ProgrammingMode.PAGEMODE);
+        Assert.assertEquals("string compare ", "V 1021", msg.toString());
+    }
+
+    @Test
+    public void testWriteCVLarge() {
+        msg = SprogMessage.getWriteCV(1021, 251, ProgrammingMode.PAGEMODE);
+        Assert.assertEquals("string compare ", "V 1021 251", msg.toString());
+    }
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        JUnitUtil.setUp();
+        m = msg = new SprogMessage(1);
+    }
+
+    @After
+    public void tearDown() {
+	m = msg = null;
+        JUnitUtil.tearDown();
+    }
+
+}
