763be803307f42fc8653421e77fdc3e9972f9ff1
Luc Esape <luc.esape@gmail.com>
2018-09-13 23:21:47 +0200

    Bug commit from the following repository nsalazar1992/Risk_Game_SE_6441 This bug commit is a reflect of source code from: http://github.com/nsalazar1992/Risk_Game_SE_6441/commit/f04457c00ebe6750a0687e56b114fc810c56cffc.

---
+++ src/test/AppTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    public AppTest() {
+    }
+
+    @Test
+    public void sanityTest() throws Exception {
+        assertEquals(5, 2 + 2);
+    }
+}
