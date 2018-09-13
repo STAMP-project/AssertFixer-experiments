120a534a27cb1a222d64d08689afa286dacbfcdb
Luc Esape <luc.esape@gmail.com>
2018-09-13 12:21:25 +0200

    Bug commit from the following repository voyages-sncf-technologies/hesperides This bug commit is a reflect of source code from: http://github.com/voyages-sncf-technologies/hesperides/commit/de96b40ae602ce6524e45b406bb17334d3d7bdcb.

---
+++ core/domain/src/main/java/org/hesperides/core/domain/platforms/PlatformProjectionRepository.java
@@ -0,0 +1,50 @@
+package org.hesperides.core.domain.platforms;
+
+import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.queryhandling.QueryHandler;
+import org.hesperides.core.domain.platforms.queries.views.ApplicationView;
+import org.hesperides.core.domain.platforms.queries.views.ModulePlatformView;
+import org.hesperides.core.domain.platforms.queries.views.PlatformView;
+import org.hesperides.core.domain.platforms.queries.views.SearchApplicationResultView;
+import org.hesperides.core.domain.platforms.queries.views.SearchPlatformResultView;
+import org.hesperides.core.domain.platforms.queries.views.properties.AbstractValuedPropertyView;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PlatformProjectionRepository {
+
+    /*** EVENT HANDLERS ***/
+
+    @EventHandler
+    void onPlatformCreatedEvent(PlatformCreatedEvent event);
+
+    @EventHandler
+    void onPlatformDeletedEvent(PlatformDeletedEvent event);
+
+    @EventHandler
+    void onPlatformUpdatedEvent(PlatformUpdatedEvent event);
+
+    /*** QUERY HANDLERS ***/
+
+    @QueryHandler
+    Boolean onPlatformExistsByKeyQuery(PlatformExistsByKeyQuery query);
+
+    @QueryHandler
+    Optional<PlatformView> onGetPlatformByKeyQuery(GetPlatformByKeyQuery query);
+
+    @QueryHandler
+    Optional<ApplicationView> onGetApplicationByNameQuery(GetApplicationByNameQuery query);
+
+    @QueryHandler
+    List<ModulePlatformView> onGetPlatformUsingModuleQuery(GetPlatformsUsingModuleQuery query);
+
+    @QueryHandler
+    List<SearchPlatformResultView> onSearchPlatformsQuery(SearchPlatformsQuery query);
+
+    @QueryHandler
+    List<SearchApplicationResultView> onSearchApplicationsQuery(SearchApplicationsQuery query);
+
+    @QueryHandler
+    List<AbstractValuedPropertyView> onGetPropertiesQuery(GetPropertiesQuery query);
+}
---
+++ core/infrastructure/src/main/java/org/hesperides/core/infrastructure/mongo/modules/MongoModuleRepository.java
@@ -0,0 +1,38 @@
+package org.hesperides.core.infrastructure.mongo.modules;
+
+import org.hesperides.core.infrastructure.mongo.templatecontainers.KeyDocument;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.hesperides.commons.spring.SpringProfiles.FAKE_MONGO;
+import static org.hesperides.commons.spring.SpringProfiles.MONGO;
+
+@Profile({MONGO, FAKE_MONGO})
+@Repository
+public interface MongoModuleRepository extends MongoRepository<ModuleDocument, String> {
+
+    ModuleDocument findByKey(KeyDocument key);
+
+    Optional<ModuleDocument> findOptionalByKey(KeyDocument key);
+
+    List<ModuleDocument> findByKeyNameAndKeyVersion(String name, String version);
+
+    List<ModuleDocument> findByKeyName(String name);
+
+    void deleteByKey(KeyDocument key);
+
+    Optional<ModuleDocument> findOptionalByKeyAndTemplatesName(KeyDocument key, String templateName);
+
+    List<ModuleDocument> findAllByKeyNameLikeAndKeyVersionLike(String name, String version, Pageable pageable);
+
+    @Query(value = "{'technos.$id': ?0}")
+    List<ModuleDocument> findAllByTechnosKey(KeyDocument technoKey);
+
+    Long countByKey(KeyDocument keyDocument);
+}
