a4e190b5ebafd8b45539b75e85500ef4dfbd8c61
Luc Esape <luc.esape@gmail.com>
2018-09-14 12:20:30 +0200

    Bug commit from the following repository voyages-sncf-technologies/hesperides This bug commit is a reflect of source code from: http://github.com/voyages-sncf-technologies/hesperides/commit/9c6ee7b058ec08ba88a3b47a213d636e186a89e8.

---
+++ core/presentation/src/main/java/org/hesperides/core/presentation/controllers/PlatformsController.java
@@ -0,0 +1,194 @@
+package org.hesperides.core.presentation.controllers;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.hesperides.core.application.platforms.PlatformUseCases;
+import org.hesperides.core.domain.modules.entities.Module;
+import org.hesperides.core.domain.platforms.entities.Platform;
+import org.hesperides.core.domain.platforms.queries.views.*;
+import org.hesperides.core.domain.platforms.queries.views.properties.AbstractValuedPropertyView;
+import org.hesperides.core.domain.templatecontainers.entities.TemplateContainer;
+import org.hesperides.core.presentation.io.platforms.*;
+import org.hesperides.core.presentation.io.platforms.properties.PropertiesOutput;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.hesperides.core.domain.security.User.fromAuthentication;
+
+@Slf4j
+@Api("/applications")
+@RequestMapping("/applications")
+@RestController
+public class PlatformsController extends AbstractController {
+
+    private final PlatformUseCases platformUseCases;
+
+    @Autowired
+    public PlatformsController(PlatformUseCases platformUseCases) {
+        this.platformUseCases = platformUseCases;
+    }
+
+    @PostMapping("/{application_name}/platforms")
+    @ApiOperation("Create platform")
+    public ResponseEntity<PlatformOutput> createPlatform(Authentication authentication,
+                                                         @PathVariable("application_name") final String applicationName,
+                                                         @Valid @RequestBody final PlatformInput platformInput) {
+
+        Platform platform = platformInput.toDomainInstance();
+        Platform.Key createdPlatformKey = platformUseCases.createPlatform(platform, fromAuthentication(authentication));
+
+        PlatformView platformView = platformUseCases.getPlatform(createdPlatformKey);
+        PlatformOutput platformOutput = new PlatformOutput(platformView);
+
+        return ResponseEntity.ok(platformOutput);
+    }
+
+    @ApiOperation("Retrieve a platform")
+    @GetMapping("/{application_name}/platforms/{platform_name}")
+    public ResponseEntity<PlatformOutput> getPlatform(@PathVariable("application_name") final String applicationName,
+                                                      @PathVariable("platform_name") final String platformName) {
+
+        Platform.Key platformKey = new Platform.Key(applicationName, platformName);
+        PlatformView platformView = platformUseCases.getPlatform(platformKey);
+        PlatformOutput platformOutput = new PlatformOutput(platformView);
+
+        return ResponseEntity.ok(platformOutput);
+    }
+
+    @ApiOperation("Update a platform")
+    @PutMapping("/{application_name}/platforms")
+    public ResponseEntity<PlatformOutput> updatePlatform(Authentication authentication,
+                                                         @PathVariable("application_name") final String applicationName,
+                                                         @RequestParam(value = "copyPropertiesForUpgradedModules", required = false) final Boolean copyProperties,
+                                                         @Valid @RequestBody final PlatformInput platformInput) {
+
+        final boolean copyRequested = Boolean.TRUE.equals(copyProperties); // no null anymore
+        Platform.Key platformKey = new Platform.Key(applicationName, platformInput.getPlatformName());
+
+        platformUseCases.updatePlatform(platformKey,
+                platformInput.toDomainInstance(),
+                copyRequested,
+                fromAuthentication(authentication)
+        );
+
+        final ResponseEntity.BodyBuilder response = ResponseEntity.status(HttpStatus.OK);
+        if (copyRequested) {
+            // TODO remove as soon as properties are handled
+            response.header("x-hesperides-warning", "no property copied! (not implemented yet)");
+        }
+
+        PlatformView platformView = platformUseCases.getPlatform(platformKey);
+        return response.body(new PlatformOutput(platformView));
+    }
+
+    @ApiOperation("Delete a platform")
+    @DeleteMapping("/{application_name}/platforms/{platform_name}")
+    public ResponseEntity deletePlatform(Authentication authentication,
+                                         @PathVariable("application_name") final String applicationName,
+                                         @PathVariable("platform_name") final String platformName) {
+
+        Platform.Key platformKey = new Platform.Key(applicationName, platformName);
+        platformUseCases.deletePlatform(platformKey, fromAuthentication(authentication));
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{application_name}")
+    @ApiOperation("Get applications")
+    public ResponseEntity<ApplicationOutput> getApplication(@PathVariable("application_name") final String applicationName,
+                                                            @RequestParam(value = "hide_platform", required = false) final Boolean hidePlatformsModules) {
+
+        ApplicationView applicationView = platformUseCases.getApplication(applicationName);
+        ApplicationOutput applicationOutput = new ApplicationOutput(applicationView, Boolean.TRUE.equals(hidePlatformsModules));
+
+        return ResponseEntity.ok(applicationOutput);
+    }
+
+    @GetMapping("/{application_name}/platforms/{platform_name}/properties/instance_model")
+    @ApiOperation("Get properties with the given path in a platform")
+    public ResponseEntity<InstanceModelOutput> getInstanceModel(Authentication authentication,
+                                                                @PathVariable("application_name") final String applicationName,
+                                                                @PathVariable(value = "platform_name") final String platform_name,
+                                                                @RequestParam(value = "path") final String path) {
+
+        Platform.Key platformKey = new Platform.Key(applicationName, platform_name);
+        Optional<InstanceModelView> instanceModelView = platformUseCases.getInstanceModel(platformKey, path, fromAuthentication(authentication));
+        Optional<InstanceModelOutput> instanceModelOutput = instanceModelView.map(InstanceModelOutput::fromInstanceView);
+        InstanceModelOutput instanceModelOutputReponse = instanceModelOutput.map(instanceModelOutput1 -> instanceModelOutput.get()).orElse(new InstanceModelOutput(new ArrayList<>()));
+
+        return ResponseEntity.ok(instanceModelOutputReponse);
+    }
+
+    @ApiOperation("Retrieve platforms using module")
+    @GetMapping("/using_module/{module_name}/{module_version}/{version_type}")
+    public ResponseEntity<List<ModulePlatformsOutput>> getPlatformsUsingModule(@PathVariable("module_name") final String moduleName,
+                                                                               @PathVariable("module_version") final String moduleVersion,
+                                                                               @PathVariable("version_type") final String moduleVersionType) {
+
+        // Exceptionnellement, le version-type fourni en paramètre est un String car on peut lui attribuer
+        // n'importe quelle valeur. Si la valeur n'est pas release, alors on considère que c'est un working-copy.
+        boolean isWorkingCopy = !TemplateContainer.VersionType.release.toString().equalsIgnoreCase(moduleVersionType);
+
+        Module.Key moduleKey = new Module.Key(moduleName, moduleVersion, TemplateContainer.getVersionType(isWorkingCopy));
+        List<ModulePlatformView> modulePlatformViews = platformUseCases.getPlatformUsingModule(moduleKey);
+        List<ModulePlatformsOutput> modulePlatformsOutputs = ModulePlatformsOutput.fromViews(modulePlatformViews);
+
+        return ResponseEntity.ok(modulePlatformsOutputs);
+    }
+
+    @ApiOperation("List platforms of a given application")
+    @PostMapping("/platforms/perform_search")
+    public ResponseEntity<List<SearchResultOutput>> searchPlatforms(@RequestParam("applicationName") final String applicationName,
+                                                                    @RequestParam(value = "platformName", required = false) final String platformName) {
+
+        this.checkQueryParameterNotEmpty("application_name", applicationName);
+        List<SearchPlatformResultView> searchPlatformResultViews = platformUseCases.searchPlatforms(applicationName, platformName);
+
+        List<SearchResultOutput> searchResultOutputs = Optional.ofNullable(searchPlatformResultViews)
+                .orElse(Collections.emptyList())
+                .stream()
+                .map(SearchResultOutput::new)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(searchResultOutputs);
+    }
+
+    @ApiOperation("Search applications")
+    @PostMapping("/perform_search")
+    public ResponseEntity<List<SearchResultOutput>> searchApplications(@RequestParam("name") final String applicationName) {
+
+        List<SearchApplicationResultView> searchApplicationResultViews = platformUseCases.searchApplications(applicationName);
+
+        List<SearchResultOutput> searchResultOutputs = Optional.ofNullable(searchApplicationResultViews)
+                .orElse(Collections.emptyList())
+                .stream()
+                .map(SearchResultOutput::new)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(searchResultOutputs);
+    }
+
+    @ApiOperation("Get properties with the given path in a platform")
+    @GetMapping("/{application_name}/platforms/{platform_name}/properties")
+    public ResponseEntity<PropertiesOutput> getProperties(Authentication authentication,
+                                                          @PathVariable("application_name") final String applicationName,
+                                                          @PathVariable("platform_name") final String platformName,
+                                                          @RequestParam(value = "path", required = false) final String path) {
+
+        Platform.Key platformKey = new Platform.Key(applicationName, platformName);
+        // TODO : gestion sécurité isProd pour cacher les propriétés de type @password
+        List<AbstractValuedPropertyView> abstractValuedPropertyViews = platformUseCases.getProperties(platformKey, path, fromAuthentication(authentication));
+        return ResponseEntity.ok(new PropertiesOutput(abstractValuedPropertyViews));
+    }
+}
---
+++ core/presentation/src/main/java/org/hesperides/core/presentation/io/platforms/properties/IterableValuedPropertyIO.java
@@ -0,0 +1,58 @@
+/*
+ *
+ * This file is part of the Hesperides distribution.
+ * (https://github.com/voyages-sncf-technologies/hesperides)
+ * Copyright (c) 2016 VSCT.
+ *
+ * Hesperides is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * Hesperides is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+package org.hesperides.core.presentation.io.platforms.properties;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.hesperides.core.domain.platforms.queries.views.properties.IterableValuedPropertyView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Value
+@EqualsAndHashCode(callSuper = true)
+public class IterableValuedPropertyIO extends AbstractValuedPropertyIO {
+
+    @SerializedName("iterable_valorisation_items")
+    List<IterablePropertyItemIO> iterablePropertyItems;
+
+    public IterableValuedPropertyIO(String name, List<IterablePropertyItemIO> iterablePropertyItems) {
+        super(name);
+        this.iterablePropertyItems = iterablePropertyItems;
+    }
+
+    public IterableValuedPropertyIO(IterableValuedPropertyView iterableValuedPropertyView) {
+        super(iterableValuedPropertyView.getName());
+        this.iterablePropertyItems = IterablePropertyItemIO.fromIterablePropertyItem(iterableValuedPropertyView.getIterablePropertyItems());
+    }
+
+    public static List<IterableValuedPropertyIO> fromIterableValuedPropertyViews(final List<IterableValuedPropertyView> iterableValuedPropertyViews) {
+        return Optional.ofNullable(iterableValuedPropertyViews)
+                .orElse(Collections.emptyList())
+                .stream()
+                .map(IterableValuedPropertyIO::new)
+                .collect(Collectors.toList());
+    }
+}
