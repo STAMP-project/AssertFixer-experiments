a69558aff5d0ab893af1c19e844443e51d69599d
Luc Esape <luc.esape@gmail.com>
2018-09-14 15:50:48 +0200

    Bug commit from the following repository voyages-sncf-technologies/hesperides This bug commit is a reflect of source code from: http://github.com/voyages-sncf-technologies/hesperides/commit/2b437677b5e82fe7ceca71896cfaedd5f18099f0.

---
+++ core/domain/src/main/java/org/hesperides/core/domain/platforms/queries/views/properties/AbstractValuedPropertyView.java
@@ -0,0 +1,44 @@
+/*
+ *
+ * This file is part of the Hesperides distribution.
+ * (https://github.com/voyages-sncf-technologies/hesperides)
+ * Copyright (c) 2016 VSCT.
+ *
+ * Hesperides is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * Hesperides is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+package org.hesperides.core.domain.platforms.queries.views.properties;
+
+import lombok.Value;
+import lombok.experimental.NonFinal;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Value
+@NonFinal
+public abstract class AbstractValuedPropertyView {
+    String name;
+
+    public static <T extends AbstractValuedPropertyView> List<T> getAbstractValuedPropertyViewWithType(final List<AbstractValuedPropertyView> abstractValuedPropertyViews, Class<T> clazz) {
+        return Optional.ofNullable(abstractValuedPropertyViews)
+                .orElse(Collections.emptyList())
+                .stream()
+                .filter(clazz::isInstance)
+                .map(clazz::cast)
+                .collect(Collectors.toList());
+    }
+}
---
+++ core/presentation/src/main/java/org/hesperides/core/presentation/io/platforms/properties/PropertiesOutput.java
@@ -0,0 +1,47 @@
+/*
+ *
+ * This file is part of the Hesperides distribution.
+ * (https://github.com/voyages-sncf-technologies/hesperides)
+ * Copyright (c) 2016 VSCT.
+ *
+ * Hesperides is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * Hesperides is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+package org.hesperides.core.presentation.io.platforms.properties;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Value;
+import org.hesperides.core.domain.platforms.queries.views.properties.AbstractValuedPropertyView;
+import org.hesperides.core.domain.platforms.queries.views.properties.IterableValuedPropertyView;
+import org.hesperides.core.domain.platforms.queries.views.properties.ValuedPropertyView;
+
+import java.util.List;
+
+@Value
+public class PropertiesOutput {
+
+    @SerializedName("key_value_properties")
+    List<ValuedPropertyIO> valuedPropertyOutputs;
+    @SerializedName("iterable_properties")
+    List<IterableValuedPropertyIO> iterableValuedPropertyOutputs;
+
+   public PropertiesOutput(List<AbstractValuedPropertyView> abstractValuedPropertyViews) {
+
+        final List<ValuedPropertyView> valuedPropertyViews = AbstractValuedPropertyView.getAbstractValuedPropertyViewWithType(abstractValuedPropertyViews, ValuedPropertyView.class);
+        this.valuedPropertyOutputs = ValuedPropertyIO.fromValuedPropertyViews(valuedPropertyViews);
+
+        final List<IterableValuedPropertyView> iterableValuedPropertyViews = AbstractValuedPropertyView.getAbstractValuedPropertyViewWithType(abstractValuedPropertyViews, IterableValuedPropertyView.class);
+        this.iterableValuedPropertyOutputs = IterableValuedPropertyIO.fromIterableValuedPropertyViews(iterableValuedPropertyViews);
+    }
+}
