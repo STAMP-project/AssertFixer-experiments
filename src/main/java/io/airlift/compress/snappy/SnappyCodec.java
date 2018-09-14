32a28946e38bd66d948300b3c593124bc730b01d
Luc Esape <luc.esape@gmail.com>
2018-09-14 08:05:40 +0200

    Bug commit from the following repository airlift/aircompressor This bug commit is a reflect of source code from: http://github.com/airlift/aircompressor/commit/77335c456943e72f4b62d736513f1e6075787c7c.

---
+++ src/main/java/io/airlift/compress/snappy/SnappyCodec.java
@@ -0,0 +1,264 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.airlift.compress.snappy;
+
+import org.apache.hadoop.conf.Configurable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.CompressionInputStream;
+import org.apache.hadoop.io.compress.CompressionOutputStream;
+import org.apache.hadoop.io.compress.Compressor;
+import org.apache.hadoop.io.compress.Decompressor;
+import org.apache.hadoop.io.compress.DoNotPool;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static org.apache.hadoop.fs.CommonConfigurationKeys.IO_COMPRESSION_CODEC_SNAPPY_BUFFERSIZE_DEFAULT;
+import static org.apache.hadoop.fs.CommonConfigurationKeys.IO_COMPRESSION_CODEC_SNAPPY_BUFFERSIZE_KEY;
+
+public class SnappyCodec
+        implements Configurable, CompressionCodec
+{
+    private Configuration conf;
+
+    @Override
+    public Configuration getConf()
+    {
+        return conf;
+    }
+
+    @Override
+    public void setConf(Configuration conf)
+    {
+        this.conf = conf;
+    }
+
+    @Override
+    public CompressionOutputStream createOutputStream(OutputStream outputStream)
+            throws IOException
+    {
+        return new HadoopSnappyOutputStream(outputStream, getBufferSize());
+    }
+
+    @Override
+    public CompressionOutputStream createOutputStream(OutputStream outputStream, Compressor compressor)
+            throws IOException
+    {
+        if (!(compressor instanceof HadoopSnappyCompressor)) {
+            throw new IllegalArgumentException("Compressor is not the Snappy decompressor");
+        }
+        return new HadoopSnappyOutputStream(outputStream, getBufferSize());
+    }
+
+    @Override
+    public Class<? extends Compressor> getCompressorType()
+    {
+        return HadoopSnappyCompressor.class;
+    }
+
+    @Override
+    public Compressor createCompressor()
+    {
+        return new HadoopSnappyCompressor();
+    }
+
+    @Override
+    public CompressionInputStream createInputStream(InputStream inputStream)
+            throws IOException
+    {
+        return new HadoopSnappyInputStream(inputStream);
+    }
+
+    @Override
+    public CompressionInputStream createInputStream(InputStream in, Decompressor decompressor)
+            throws IOException
+    {
+        if (!(decompressor instanceof HadoopSnappyDecompressor)) {
+            throw new IllegalArgumentException("Decompressor is not the Snappy decompressor");
+        }
+        return new HadoopSnappyInputStream(in);
+    }
+
+    @Override
+    public Class<? extends Decompressor> getDecompressorType()
+    {
+        return HadoopSnappyDecompressor.class;
+    }
+
+    @Override
+    public Decompressor createDecompressor()
+    {
+        return new HadoopSnappyDecompressor();
+    }
+
+    @Override
+    public String getDefaultExtension()
+    {
+        return ".snappy";
+    }
+
+    private int getBufferSize()
+    {
+        // Favor using the configured buffer size.  This is not as critical for Snappy
+        // since Snappy always writes the compressed chunk size, so we always know the
+        // correct buffer size to create.
+        int maxUncompressedLength;
+        if (conf != null) {
+            maxUncompressedLength = conf.getInt(IO_COMPRESSION_CODEC_SNAPPY_BUFFERSIZE_KEY, IO_COMPRESSION_CODEC_SNAPPY_BUFFERSIZE_DEFAULT);
+        }
+        else {
+            maxUncompressedLength = IO_COMPRESSION_CODEC_SNAPPY_BUFFERSIZE_DEFAULT;
+        }
+        return maxUncompressedLength;
+    }
+
+    /**
+     * No Hadoop code seems to actually use the compressor, so just return a dummy one so the createOutputStream method
+     * with a compressor can function.  This interface can be implemented if needed.
+     */
+    @DoNotPool
+    private static class HadoopSnappyCompressor
+            implements Compressor
+    {
+        @Override
+        public void setInput(byte[] b, int off, int len)
+        {
+            throw new UnsupportedOperationException("Snappy block compressor is not supported");
+        }
+
+        @Override
+        public boolean needsInput()
+        {
+            throw new UnsupportedOperationException("Snappy block compressor is not supported");
+        }
+
+        @Override
+        public void setDictionary(byte[] b, int off, int len)
+        {
+            throw new UnsupportedOperationException("Snappy block compressor is not supported");
+        }
+
+        @Override
+        public long getBytesRead()
+        {
+            throw new UnsupportedOperationException("Snappy block compressor is not supported");
+        }
+
+        @Override
+        public long getBytesWritten()
+        {
+            throw new UnsupportedOperationException("Snappy block compressor is not supported");
+        }
+
+        @Override
+        public void finish()
+        {
+            throw new UnsupportedOperationException("Snappy block compressor is not supported");
+        }
+
+        @Override
+        public boolean finished()
+        {
+            throw new UnsupportedOperationException("Snappy block compressor is not supported");
+        }
+
+        @Override
+        public int compress(byte[] b, int off, int len)
+                throws IOException
+        {
+            throw new UnsupportedOperationException("Snappy block compressor is not supported");
+        }
+
+        @Override
+        public void reset()
+        {
+        }
+
+        @Override
+        public void end()
+        {
+            throw new UnsupportedOperationException("Snappy block compressor is not supported");
+        }
+
+        @Override
+        public void reinit(Configuration conf)
+        {
+        }
+    }
+
+    /**
+     * No Hadoop code seems to actually use the decompressor, so just return a dummy one so the createInputStream method
+     * with a decompressor can function.  This interface can be implemented if needed.
+     */
+    @DoNotPool
+    private static class HadoopSnappyDecompressor
+            implements Decompressor
+    {
+        @Override
+        public void setInput(byte[] b, int off, int len)
+        {
+            throw new UnsupportedOperationException("Snappy block decompressor is not supported");
+        }
+
+        @Override
+        public boolean needsInput()
+        {
+            throw new UnsupportedOperationException("Snappy block decompressor is not supported");
+        }
+
+        @Override
+        public void setDictionary(byte[] b, int off, int len)
+        {
+            throw new UnsupportedOperationException("Snappy block decompressor is not supported");
+        }
+
+        @Override
+        public boolean needsDictionary()
+        {
+            throw new UnsupportedOperationException("Snappy block decompressor is not supported");
+        }
+
+        @Override
+        public boolean finished()
+        {
+            throw new UnsupportedOperationException("Snappy block decompressor is not supported");
+        }
+
+        @Override
+        public int decompress(byte[] b, int off, int len)
+                throws IOException
+        {
+            throw new UnsupportedOperationException("Snappy block decompressor is not supported");
+        }
+
+        @Override
+        public void reset()
+        {
+        }
+
+        @Override
+        public int getRemaining()
+        {
+            throw new UnsupportedOperationException("Snappy block decompressor is not supported");
+        }
+
+        @Override
+        public void end()
+        {
+            throw new UnsupportedOperationException("Snappy block decompressor is not supported");
+        }
+    }
+}
---
+++ src/test/java/io/airlift/compress/benchmark/Algorithm.java
@@ -0,0 +1,97 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.airlift.compress.benchmark;
+
+import io.airlift.compress.Compressor;
+import io.airlift.compress.Decompressor;
+import io.airlift.compress.DeflateCompressor;
+import io.airlift.compress.HadoopCodecCompressor;
+import io.airlift.compress.HadoopCodecDecompressor;
+import io.airlift.compress.InflateDecompressor;
+import io.airlift.compress.lz4.Lz4Codec;
+import io.airlift.compress.lz4.Lz4Compressor;
+import io.airlift.compress.lz4.Lz4Decompressor;
+import io.airlift.compress.lzo.LzoCodec;
+import io.airlift.compress.lzo.LzoCompressor;
+import io.airlift.compress.lzo.LzoDecompressor;
+import io.airlift.compress.snappy.SnappyCodec;
+import io.airlift.compress.snappy.SnappyCompressor;
+import io.airlift.compress.snappy.SnappyDecompressor;
+import io.airlift.compress.thirdparty.HadoopLzoCompressor;
+import io.airlift.compress.thirdparty.HadoopLzoDecompressor;
+import io.airlift.compress.thirdparty.Iq80SnappyCompressor;
+import io.airlift.compress.thirdparty.Iq80SnappyDecompressor;
+import io.airlift.compress.thirdparty.JPountzLz4JniCompressor;
+import io.airlift.compress.thirdparty.JPountzLz4JniDecompressor;
+import io.airlift.compress.thirdparty.XerialSnappyCompressor;
+import io.airlift.compress.thirdparty.XerialSnappyDecompressor;
+import io.airlift.compress.thirdparty.ZstdJniCompressor;
+import io.airlift.compress.thirdparty.ZstdJniDecompressor;
+import io.airlift.compress.zstd.ZstdDecompressor;
+import org.apache.hadoop.conf.Configurable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.compress.CompressionCodec;
+
+public enum Algorithm
+{
+    airlift_lz4(new Lz4Decompressor(), new Lz4Compressor()),
+    airlift_snappy(new SnappyDecompressor(), new SnappyCompressor()),
+    airlift_lzo(new LzoDecompressor(), new LzoCompressor()),
+    airlift_zstd(new ZstdDecompressor(), new ZstdJniCompressor(6)),
+
+    airlift_lz4_stream(new Lz4Codec(), new Lz4Compressor()),
+    airlift_snappy_stream(new SnappyCodec(), new SnappyCompressor()),
+    airlift_lzo_stream(new LzoCodec(), new LzoCompressor()),
+
+    jpountz_lz4_jni(new JPountzLz4JniDecompressor(), new JPountzLz4JniCompressor()),
+    xerial_snappy(new XerialSnappyDecompressor(), new XerialSnappyCompressor()),
+    iq80_snappy(new Iq80SnappyDecompressor(), new Iq80SnappyCompressor()),
+    hadoop_lzo(new HadoopLzoDecompressor(), new HadoopLzoCompressor()),
+    zstd_jni(new ZstdJniDecompressor(), new ZstdJniCompressor(6)),
+
+    hadoop_lz4_stream(new org.apache.hadoop.io.compress.Lz4Codec(), new Lz4Compressor()),
+    hadoop_snappy_stream(new org.apache.hadoop.io.compress.SnappyCodec(), new SnappyCompressor()),
+    hadoop_lzo_stream(new com.hadoop.compression.lzo.LzoCodec(), new LzoCompressor()),
+
+    java_zip_stream(new InflateDecompressor(), new DeflateCompressor()),
+    hadoop_gzip_stream(new org.apache.hadoop.io.compress.GzipCodec(), new LzoCompressor());
+
+    private final Decompressor decompressor;
+    private final Compressor compressor;
+
+    Algorithm(CompressionCodec compressionCodec, Compressor compressor)
+    {
+        if (compressionCodec instanceof Configurable) {
+            ((Configurable) compressionCodec).setConf(new Configuration());
+        }
+        this.decompressor = new HadoopCodecDecompressor(compressionCodec);
+        this.compressor = new HadoopCodecCompressor(compressionCodec, compressor);
+    }
+
+    Algorithm(Decompressor decompressor, Compressor compressor)
+    {
+        this.decompressor = decompressor;
+        this.compressor = compressor;
+    }
+
+    public Compressor getCompressor()
+    {
+        return compressor;
+    }
+
+    public Decompressor getDecompressor()
+    {
+        return decompressor;
+    }
+}
