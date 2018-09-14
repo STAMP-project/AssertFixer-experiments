f4d84be882ed0c714a7bfc46821c50517d4c6793
Luc Esape <luc.esape@gmail.com>
2018-09-14 11:49:16 +0200

    Bug commit from the following repository voyages-sncf-technologies/hesperides This bug commit is a reflect of source code from: http://github.com/voyages-sncf-technologies/hesperides/commit/e7d4c006b8cb1e68a0523fe9ede11b0df8c63d01.

---
+++ core/domain/src/main/java/org/hesperides/core/domain/platforms/PlatformProjectionRepository.java
@@ -0,0 +1,58 @@
+package org.hesperides.core.domain.platforms;
+
+import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.queryhandling.QueryHandler;
+import org.hesperides.core.domain.platforms.queries.views.ApplicationView;
+import org.hesperides.core.domain.platforms.queries.views.ModulePlatformView;
+import org.hesperides.core.domain.platforms.queries.views.PlatformView;
+import org.hesperides.core.domain.platforms.queries.views.SearchApplicationResultView;
+import org.hesperides.core.domain.platforms.queries.views.SearchPlatformResultView;
+import org.hesperides.core.domain.platforms.queries.views.InstanceModelView;
+import org.hesperides.core.domain.platforms.queries.views.properties.AbstractValuedPropertyView;
+import org.hesperides.core.domain.platforms.queries.views.properties.ValuedPropertyView;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PlatformProjectionRepository {
+
+    /*** EVENT HANDLERS ***/
+
+    @EventHandler
+    void onPlatformCreatedEvent(PlatformCreatedEvent event);
+
+    @EventHandler
+    void onPlatformDeletedEvent(PlatformDeletedEvent event);
+
+    @EventHandler
+    void onPlatformUpdatedEvent(PlatformUpdatedEvent event);
+
+    /*** QUERY HANDLERS ***/
+
+    @QueryHandler
+    Boolean onPlatformExistsByKeyQuery(PlatformExistsByKeyQuery query);
+
+    @QueryHandler
+    Optional<PlatformView> onGetPlatformByKeyQuery(GetPlatformByKeyQuery query);
+
+    @QueryHandler
+    Optional<ApplicationView> onGetApplicationByNameQuery(GetApplicationByNameQuery query);
+
+    @QueryHandler
+    Optional<InstanceModelView> onGetInstanceModelQuery(GetInstanceModelQuery query);
+
+    @QueryHandler
+    List<ModulePlatformView> onGetPlatformUsingModuleQuery(GetPlatformsUsingModuleQuery query);
+
+    @QueryHandler
+    List<SearchPlatformResultView> onSearchPlatformsQuery(SearchPlatformsQuery query);
+
+    @QueryHandler
+    List<SearchApplicationResultView> onSearchApplicationsQuery(SearchApplicationsQuery query);
+
+    @QueryHandler
+    List<AbstractValuedPropertyView> onGetDeployedModulePropertiesQuery(GetDeployedModulesPropertiesQuery query);
+
+    @QueryHandler
+    List<ValuedPropertyView> onGetGlobalPropertiesQuery(GetGlobalPropertiesQuery query);
+}
