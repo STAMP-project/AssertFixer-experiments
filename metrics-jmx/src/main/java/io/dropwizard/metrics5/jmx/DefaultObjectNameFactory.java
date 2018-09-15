57ff58be3e9edd07296dd5019a4a39915dbcd57e
Luc Esape <luc.esape@gmail.com>
2018-09-15 19:55:15 +0200

    Bug commit from the following repository dropwizard/metrics This bug commit is a reflect of source code from: http://github.com/dropwizard/metrics/commit/fda142774f156b091ecfbb8386de1f7eedc3b083.

---
+++ metrics-jmx/src/main/java/io/dropwizard/metrics5/jmx/DefaultObjectNameFactory.java
@@ -0,0 +1,52 @@
+package io.dropwizard.metrics5.jmx;
+
+import io.dropwizard.metrics5.MetricName;
+import java.util.Hashtable;
+
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DefaultObjectNameFactory implements ObjectNameFactory {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JmxReporter.class);
+
+    @Override
+    public ObjectName createName(String type, String domain, MetricName name) {
+        try {
+            ObjectName objectName;
+            Hashtable<String, String> properties = new Hashtable<>();
+
+            properties.put("name", name.getKey());
+            properties.put("type", type);
+            objectName = new ObjectName(domain, properties);
+
+            /*
+             * The only way we can find out if we need to quote the properties is by
+             * checking an ObjectName that we've constructed.
+             */
+            if (objectName.isDomainPattern()) {
+                domain = ObjectName.quote(domain);
+            }
+            if (objectName.isPropertyValuePattern("name")) {
+                properties.put("name", ObjectName.quote(name.getKey()));
+            }
+            if (objectName.isPropertyValuePattern("type")) {
+                properties.put("type", ObjectName.quote(type));
+            }
+            objectName = new ObjectName(domain, properties);
+
+            return objectName;
+        } catch (MalformedObjectNameException e) {
+            try {
+                return new ObjectName(domain, "name", ObjectName.quote(name.getKey()));
+            } catch (MalformedObjectNameException e1) {
+                LOGGER.warn("Unable to register {} {}", type, name, e1);
+                throw new RuntimeException(e1);
+            }
+        }
+    }
+
+}
