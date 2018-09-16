f6217b3ff114b4042ae7d63ae24d2215b9e0ec6a
Luc Esape <luc.esape@gmail.com>
2018-09-16 20:25:14 +0200

    Bug commit from the following repository JMRI/JMRI This bug commit is a reflect of source code from: http://github.com/JMRI/JMRI/commit/45f59e9319e8ca03d18825712ee7951c9625f6de.

---
+++ java/src/jmri/jmrix/srcp/swing/srcpmon/SRCPMonAction.java
@@ -0,0 +1,19 @@
+package jmri.jmrix.srcp.swing.srcpmon;
+
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import jmri.jmrix.srcp.SRCPSystemConnectionMemo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Swing action to create and register a SRCPMonFrame object
+ *
+ * @author Bob Jacobsen Copyright (C) 2008
+ * @author Bob Jacobsen Copyright (C) 2018
+ */
+public class SRCPMonAction extends SRCPMonPane.Default {
+}
+
+
+
