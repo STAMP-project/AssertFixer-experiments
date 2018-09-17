2cf3fdf23bd78ac517a729f356a73257c309c142
Luc Esape <luc.esape@gmail.com>
2018-09-17 03:51:30 +0200

    Bug commit from the following repository Microsoft/spring-data-gremlin This bug commit is a reflect of source code from: http://github.com/Microsoft/spring-data-gremlin/commit/e3be38ecd76f340d788710a6a36360a70ea2944a.

---
+++ src/main/java/com/microsoft/spring/data/gremlin/common/GremlinFactory.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.spring.data.gremlin.common;
+
+import com.microsoft.spring.data.gremlin.exception.GremlinIllegalConfigurationException;
+import com.microsoft.spring.data.gremlin.telemetry.TelemetryTracker;
+import lombok.Getter;
+import org.apache.tinkerpop.gremlin.driver.Client;
+import org.apache.tinkerpop.gremlin.driver.Cluster;
+import org.apache.tinkerpop.gremlin.driver.ser.Serializers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+
+public class GremlinFactory {
+
+    @Getter
+    private Cluster gremlinCluster;
+
+    private GremlinConfig gremlinConfig;
+
+    @Autowired
+    private TelemetryTracker telemetryTracker;
+
+    public GremlinFactory(@NonNull GremlinConfig gremlinConfig) {
+        final int port = gremlinConfig.getPort();
+        if (port <= 0 || port > 65535) {
+            gremlinConfig.setPort(Constants.DEFAULT_ENDPOINT_PORT);
+        }
+
+        this.gremlinConfig = gremlinConfig;
+    }
+
+    private void trackTelemetryCustomEvent() {
+        this.telemetryTracker.trackEvent(getClass().getSimpleName());
+    }
+
+    private Cluster createGremlinCluster() throws GremlinIllegalConfigurationException {
+        final Cluster cluster;
+
+        try {
+            cluster = Cluster.build(this.gremlinConfig.getEndpoint())
+                    .serializer(Serializers.DEFAULT_RESULT_SERIALIZER)
+                    .credentials(this.gremlinConfig.getUsername(), this.gremlinConfig.getPassword())
+                    .enableSsl(true)
+                    .port(this.gremlinConfig.getPort())
+                    .create();
+        } catch (IllegalArgumentException e) {
+            throw new GremlinIllegalConfigurationException("Invalid configuration of Gremlin", e);
+        }
+
+        trackTelemetryCustomEvent();
+
+        return cluster;
+    }
+
+    public Client getGremlinClient() {
+
+        if (this.gremlinCluster == null) {
+            this.gremlinCluster = this.createGremlinCluster();
+        }
+
+        return this.gremlinCluster.connect();
+    }
+}
