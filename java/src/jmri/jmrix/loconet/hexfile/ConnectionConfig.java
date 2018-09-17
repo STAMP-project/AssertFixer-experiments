4e3ce12bb70f01e58c0cff1d92a7f73605379d0b
Luc Esape <luc.esape@gmail.com>
2018-09-18 01:13:47 +0200

    Bug commit from the following repository pabender/JMRI This bug commit is a reflect of source code from: http://github.com/pabender/JMRI/commit/9aa063ed2aa62cb4dfed59ca5b836bcf6c524ebf.

---
+++ java/src/jmri/jmrix/loconet/hexfile/ConnectionConfig.java
@@ -0,0 +1,39 @@
+package jmri.jmrix.loconet.hexfile;
+
+/**
+ * Definition of objects to handle configuring a layout connection via a LocoNet
+ * hexfile emulator.
+ *
+ * @author Bob Jacobsen Copyright (C) 2001, 2003
+ */
+public class ConnectionConfig extends jmri.jmrix.AbstractSimulatorConnectionConfig {
+
+    /**
+     * Ctor for an object being created during load process; Swing init is
+     * deferred.
+     * <p>
+     * @param p - an {@link jmri.jmrix.SerialPortAdapter} object
+     */
+    public ConnectionConfig(jmri.jmrix.SerialPortAdapter p) {
+        super(p);
+    }
+
+    /**
+     */
+    public ConnectionConfig() {
+        super();
+    }
+
+    @Override
+    public String name() {
+        return "LocoNet Simulator"; // NOI18N
+    }
+
+    @Override
+    protected void setInstance() {
+        if (adapter == null) {
+            adapter = new LnHexFilePort();
+        }
+    }
+
+}
