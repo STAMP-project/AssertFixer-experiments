17f0471fb2dea9efe92c97daff8f70494b0b5c14
Luc Esape <luc.esape@gmail.com>
2018-09-18 17:45:29 +0200

    Bug commit from the following repository bobjacobsen/JMRI This bug commit is a reflect of source code from: http://github.com/bobjacobsen/JMRI/commit/b1b4e25cd67d3522e97633cd8e2be09521e2cea2.

---
+++ java/test/jmri/ConditionalVariableTest.java
@@ -0,0 +1,53 @@
+package jmri;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Tests for the Path class
+ *
+ * @author	Bob Jacobsen Copyright (C) 2016
+ */
+public class ConditionalVariableTest {
+
+    @Test
+    public void testConstants() {
+        // It might be a good idea to change constants into enums.
+        // These tests ensures that the values of the constants stay the same
+        // if that change is done.
+        
+        Assert.assertEquals(ConditionalVariable.NUM_COMPARE_OPERATIONS, 5);
+        Assert.assertEquals(ConditionalVariable.LESS_THAN, 1);
+        Assert.assertEquals(ConditionalVariable.LESS_THAN_OR_EQUAL, 2);
+        Assert.assertEquals(ConditionalVariable.EQUAL, 3);
+        Assert.assertEquals(ConditionalVariable.GREATER_THAN_OR_EQUAL, 4);
+        Assert.assertEquals(ConditionalVariable.GREATER_THAN, 5);
+    }
+    
+    @Test
+    public void testEquals() {
+        ConditionalVariable c1 = new ConditionalVariable(false, 1, 2, "name", false);
+        ConditionalVariable c2 = new ConditionalVariable(false, 1, 2, "name", false);
+
+        Assert.assertTrue("identity", c1.equals(c1));
+        Assert.assertFalse("object equals, not content equals", c1.equals(c2));
+    }
+
+    
+    // from here down is testing infrastructure
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        jmri.util.JUnitUtil.setUp();
+        jmri.InstanceManager.store(new jmri.NamedBeanHandleManager(), jmri.NamedBeanHandleManager.class);
+    }
+
+    @After
+    public void tearDown() {
+        jmri.util.JUnitUtil.tearDown();
+    }
+}
