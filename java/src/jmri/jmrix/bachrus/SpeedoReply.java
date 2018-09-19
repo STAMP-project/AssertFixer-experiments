d5430024313c28e2a06859a06e5585f5e2f0e907
Luc Esape <luc.esape@gmail.com>
2018-09-19 07:49:01 +0200

    Bug commit from the following repository pabender/JMRI This bug commit is a reflect of source code from: http://github.com/pabender/JMRI/commit/f0a54cc6069b715f3b96c5a5935016fb7060befb.

---
+++ java/src/jmri/jmrix/bachrus/SpeedoReply.java
@@ -0,0 +1,104 @@
+package jmri.jmrix.bachrus;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Carries the reply to an SprogMessage
+ *
+ * @author Bob Jacobsen Copyright (C) 2001
+ * @author Andrew Crosland Copyright (C) 2010
+ */
+public class SpeedoReply extends jmri.jmrix.AbstractMRReply {
+ // This should be an extension af AbstractMRReply and needs re-factoring
+
+    // create a new one
+    public SpeedoReply() {
+	super();
+    }
+
+    // copy one
+    @SuppressWarnings("null")
+    public SpeedoReply(SpeedoReply m) {
+        this();
+        if (m == null) {
+            log.error("copy ctor of null message");
+            return;
+        }
+        _nDataChars = m._nDataChars;
+	if(m.isUnsolicited()) {
+           setUnsolicited();
+	}
+        for (int i = 0; i < _nDataChars; i++) {
+            _dataChars[i] = m._dataChars[i];
+        }
+    }
+
+    // from String
+    public SpeedoReply(String s) {
+        this();
+        _nDataChars = s.length();
+        for (int i = 0; i < _nDataChars; i++) {
+            _dataChars[i] = s.charAt(i);
+        }
+    }
+
+    public int getCount() {
+        // don't return 0 as it will cause an exception
+        if (_nDataChars < 9) {
+            return -1;
+        }
+        try {
+            return Integer.parseInt(this.toString().substring(2, 8), 16);
+        } catch (NumberFormatException ex) {
+            return 0;
+        }
+    }
+
+    public int getSeries() {
+        if (_nDataChars < 7) {
+            return 0;
+        }
+        try {
+            return Integer.parseInt(this.toString().substring(1, 2));
+        } catch (NumberFormatException ex) {
+            return 0;
+        }
+    }
+
+    // display format
+    @Override
+    public String toString() {
+        StringBuffer buf = new StringBuffer();
+        for (int i = 0; i < _nDataChars; i++) {
+            buf.append(_dataChars[i]);
+        }
+        return buf.toString();
+    }
+
+    @Override
+    public int match(String s) {
+        // find a specific string in the reply
+        String rep = new String(_dataChars, 0, _nDataChars);
+        return rep.indexOf(s);
+    }
+
+    static public final int maxSize = 32;
+
+    @Override
+    public int maxSize() {
+        return maxSize;
+    }
+
+    /**
+     * skipPrefix is not used at this point in time, but is 
+     * defined as abstract in AbstractMRReply
+     */
+    @Override
+    protected int skipPrefix(int index) {
+        return -1;
+    }
+
+    private final static Logger log = LoggerFactory.getLogger(SpeedoReply.class);
+
+}
