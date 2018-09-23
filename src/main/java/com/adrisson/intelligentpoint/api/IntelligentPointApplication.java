00372ecb20495f8e45dcf4968ea223489c65a633
Luc Esape <luc.esape@gmail.com>
2018-09-23 02:57:16 +0200

    Bug commit from the following repository adrissonGalvao/intelligent-point This bug commit is a reflect of source code from: http://github.com/adrissonGalvao/intelligent-point/commit/de919fd6620f14df04aa97e6072f4e60b2359266.

---
+++ src/main/java/com/adrisson/intelligentpoint/api/IntelligentPointApplication.java
@@ -0,0 +1,12 @@
+package com.adrisson.intelligentpoint.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentPointApplication.class, args);
+	}
+}
