8d833be29b6343e944c012b8221517f5b4102f7d
Luc Esape <luc.esape@gmail.com>
2018-09-26 05:09:47 +0200

    Bug commit from the following repository JMRI/JMRI This bug commit is a reflect of source code from: http://github.com/JMRI/JMRI/commit/d7166cc5e248340619f4b703378d0b40b47ac282.

---
+++ java/test/jmri/jmrit/throttle/FunctionPanelTest.java
@@ -0,0 +1,44 @@
+package jmri.jmrit.throttle;
+
+import java.awt.GraphicsEnvironment;
+import jmri.util.JUnitUtil;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test simple functioning of FunctionPanel
+ *
+ * @author	Paul Bender Copyright (C) 2016
+ */
+public class FunctionPanelTest {
+
+    @Test
+    public void testCtor() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        FunctionPanel frame = new FunctionPanel(); // not a panel despite class name
+        Assert.assertNotNull("exists", frame);
+    }
+
+    @Test
+    public void testGetFunctionButtons(){
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        FunctionPanel frame = new FunctionPanel(); // not a panel despite class name
+        FunctionButton fba[] = frame.getFunctionButtons();
+	Assert.assertNotNull("Function Button Array exists",fba);
+	Assert.assertEquals("Function Button Array has right length",29,fba.length);
+    }
+
+    @Before
+    public void setUp() {
+        JUnitUtil.setUp();
+        JUnitUtil.resetProfileManager();
+    }
+
+    @After
+    public void tearDown() {
+        JUnitUtil.tearDown();
+    }
+}
