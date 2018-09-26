a90238bd772aaed32546db93026a581a935d8327
Luc Esape <luc.esape@gmail.com>
2018-09-26 15:30:56 +0200

    Bug commit from the following repository project-sunbird/open-saber This bug commit is a reflect of source code from: http://github.com/project-sunbird/open-saber/commit/2bec11fcd02a2e5304e87b99c53dcf5d3cf6b0ba.

---
+++ java/registry/src/main/java/io/opensaber/registry/dao/RegistryDao.java
@@ -0,0 +1,28 @@
+package io.opensaber.registry.dao;
+
+import io.opensaber.registry.exception.AuditFailedException;
+import io.opensaber.registry.exception.DuplicateRecordException;
+import io.opensaber.registry.exception.EncryptionException;
+import io.opensaber.registry.exception.RecordNotFoundException;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public interface RegistryDao {
+
+	public List getEntityList();
+
+	public String addEntity(Graph entity, String label, String rootNodeLabel, String property) throws DuplicateRecordException, RecordNotFoundException, NoSuchElementException, EncryptionException, AuditFailedException;
+
+	public boolean updateEntity(Graph entityForUpdate, String rootNodeLabel, String methodOrigin)
+			throws RecordNotFoundException, NoSuchElementException, EncryptionException, AuditFailedException;
+
+    public Graph getEntityById(String label, boolean includeSignatures) throws RecordNotFoundException, NoSuchElementException, EncryptionException, AuditFailedException;
+	
+	public Graph getEntityByVertex(Vertex vertex) throws RecordNotFoundException, NoSuchElementException, EncryptionException, AuditFailedException;
+
+	public boolean deleteEntityById (String id) throws RecordNotFoundException,AuditFailedException;
+
+}
