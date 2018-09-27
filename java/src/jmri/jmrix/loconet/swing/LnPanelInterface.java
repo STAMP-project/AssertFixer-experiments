28ce45f7b35f24c45213bb6ca94d64857d447272
Luc Esape <luc.esape@gmail.com>
2018-09-27 04:50:16 +0200

    Bug commit from the following repository JMRI/JMRI This bug commit is a reflect of source code from: http://github.com/JMRI/JMRI/commit/cc776ae4c1d0d6372246a83fb0ee7ad49d0e0559.

---
+++ java/src/jmri/jmrix/loconet/swing/LnPanelInterface.java
@@ -0,0 +1,24 @@
+package jmri.jmrix.loconet.swing;
+
+import jmri.jmrix.loconet.LocoNetSystemConnectionMemo;
+
+/**
+ * JPanel interface to handle providing system connection information to a
+ * panel.
+ *
+ * @author Bob Jacobsen Copyright 2010
+ * @since 2.9.4
+ */
+public interface LnPanelInterface {
+
+    /**
+     * 2nd stage of initialization, invoked after the constuctor is complete.
+     * <p>
+     * This needs to be connected to the initContext() method in implementing
+     * classes.
+     * <p>
+     * @param memo - a {@link jmri.jmrix.loconet.LocoNetSystemConnectionMemo} object
+     */
+    public void initComponents(LocoNetSystemConnectionMemo memo);
+
+}
