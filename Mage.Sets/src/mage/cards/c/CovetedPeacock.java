df7168c3676744f1c4aff8cf189f498d3474f09b
Luc Esape <luc.esape@gmail.com>
2018-09-27 04:33:47 +0200

    Bug commit from the following repository magefree/mage This bug commit is a reflect of source code from: http://github.com/magefree/mage/commit/a5c842740c5a579e5bcd4d11727473c75e4ac1f5.

---
+++ Mage.Sets/src/mage/cards/c/CovetedPeacock.java
@@ -0,0 +1,55 @@
+
+package mage.cards.c;
+
+import mage.MageInt;
+import mage.abilities.Ability;
+import mage.abilities.common.AttacksTriggeredAbility;
+import mage.abilities.effects.common.combat.GoadTargetEffect;
+import mage.abilities.keyword.FlyingAbility;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.filter.FilterPermanent;
+import mage.filter.common.FilterCreaturePermanent;
+import mage.filter.predicate.permanent.DefendingPlayerControlsPredicate;
+import mage.target.TargetPermanent;
+
+import java.util.UUID;
+
+/**
+ * @author TheElk801
+ */
+public final class CovetedPeacock extends CardImpl {
+
+    public static final FilterPermanent filter = new FilterCreaturePermanent("creature defending player controls");
+
+    static {
+        filter.add(new DefendingPlayerControlsPredicate());
+    }
+
+    public CovetedPeacock(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{3}{U}{U}");
+
+        this.subtype.add(SubType.BIRD);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(4);
+
+        // Flying
+        this.addAbility(FlyingAbility.getInstance());
+
+        // Whenever Coveted Peacock attacks, you may goad target creature defending player controls.
+        Ability ability = new AttacksTriggeredAbility(new GoadTargetEffect(), true, "Whenever {this} attacks, you may goad target creature defending player controls.");
+        ability.addTarget(new TargetPermanent(filter));
+        this.addAbility(ability);
+    }
+
+    public CovetedPeacock(final CovetedPeacock card) {
+        super(card);
+    }
+
+    @Override
+    public CovetedPeacock copy() {
+        return new CovetedPeacock(this);
+    }
+}
---
+++ Mage.Sets/src/mage/cards/l/LilianaDefiantNecromancer.java
@@ -0,0 +1,86 @@
+package mage.cards.l;
+
+import java.util.UUID;
+import mage.abilities.Ability;
+import mage.abilities.LoyaltyAbility;
+import mage.abilities.common.PlanswalkerEntersWithLoyalityCountersAbility;
+import mage.abilities.costs.Cost;
+import mage.abilities.costs.common.PayVariableLoyaltyCost;
+import mage.abilities.effects.common.GetEmblemEffect;
+import mage.abilities.effects.common.ReturnFromGraveyardToBattlefieldTargetEffect;
+import mage.abilities.effects.common.discard.DiscardEachPlayerEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.*;
+import mage.filter.FilterCard;
+import mage.filter.common.FilterCreatureCard;
+import mage.filter.predicate.Predicates;
+import mage.filter.predicate.mageobject.ConvertedManaCostPredicate;
+import mage.filter.predicate.mageobject.SupertypePredicate;
+import mage.game.Game;
+import mage.game.command.emblems.LilianaDefiantNecromancerEmblem;
+import mage.target.common.TargetCardInYourGraveyard;
+import mage.target.targetadjustment.TargetAdjuster;
+
+/**
+ *
+ * @author LevelX2
+ */
+public final class LilianaDefiantNecromancer extends CardImpl {
+
+    protected static final FilterCreatureCard filter = new FilterCreatureCard("nonlegendary creature with converted mana cost X from your graveyard");
+
+    static {
+        filter.add(Predicates.not(new SupertypePredicate(SuperType.LEGENDARY)));
+    }
+
+    public LilianaDefiantNecromancer(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.PLANESWALKER}, "");
+        this.addSuperType(SuperType.LEGENDARY);
+        this.subtype.add(SubType.LILIANA);
+        this.color.setBlack(true);
+
+        this.nightCard = true;
+
+        this.addAbility(new PlanswalkerEntersWithLoyalityCountersAbility(3));
+
+        // +2: Each player discards a card.
+        this.addAbility(new LoyaltyAbility(new DiscardEachPlayerEffect(1, false), 2));
+
+        // -X: Return target nonlegendary creature with converted mana cost X from your graveyard to the battlefield.
+        Ability ability = new LoyaltyAbility(new ReturnFromGraveyardToBattlefieldTargetEffect());
+        ability.addTarget(new TargetCardInYourGraveyard(filter));
+        ability.setTargetAdjuster(LilianaDefiantNecromancerAdjuster.instance);
+        this.addAbility(ability);
+
+        //-8: You get an emblem with "Whenever a creature dies, return it to the battlefield under your control at the beginning of the next end step.";
+        this.addAbility(new LoyaltyAbility(new GetEmblemEffect(new LilianaDefiantNecromancerEmblem()), -8));
+    }
+
+    public LilianaDefiantNecromancer(final LilianaDefiantNecromancer card) {
+        super(card);
+    }
+
+    @Override
+    public LilianaDefiantNecromancer copy() {
+        return new LilianaDefiantNecromancer(this);
+    }
+}
+
+enum LilianaDefiantNecromancerAdjuster implements TargetAdjuster {
+    instance;
+
+    @Override
+    public void adjustTargets(Ability ability, Game game) {
+        int cmc = 0;
+        for (Cost cost : ability.getCosts()) {
+            if (cost instanceof PayVariableLoyaltyCost) {
+                cmc = ((PayVariableLoyaltyCost) cost).getAmount();
+            }
+        }
+        FilterCard newFilter = LilianaDefiantNecromancer.filter.copy();
+        newFilter.add(new ConvertedManaCostPredicate(ComparisonType.EQUAL_TO, cmc));
+        ability.getTargets().clear();
+        ability.addTarget(new TargetCardInYourGraveyard(newFilter));
+    }
+}
