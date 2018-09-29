7863f271c24c6ed2309511a641c24570b3ef8871
Luc Esape <luc.esape@gmail.com>
2018-09-29 15:35:57 +0200

    Bug commit from the following repository pabender/JMRI This bug commit is a reflect of source code from: http://github.com/pabender/JMRI/commit/46ef143e961c3a8c9366048027b53ef7f9c5d7bc.

---
+++ java/src/jmri/jmrix/srcp/SRCPTurnoutManager.java
@@ -0,0 +1,51 @@
+package jmri.jmrix.srcp;
+
+import jmri.Turnout;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Implement turnout manager for SRCP systems
+ * <P>
+ * System names are "DTnnn", where nnn is the turnout number without padding.
+ *
+ * @author	Bob Jacobsen Copyright (C) 2001, 2008
+ */
+public class SRCPTurnoutManager extends jmri.managers.AbstractTurnoutManager {
+
+    int _bus = 0;
+    SRCPBusConnectionMemo _memo = null;
+
+    @Deprecated
+    public SRCPTurnoutManager() {
+
+    }
+
+    public SRCPTurnoutManager(SRCPBusConnectionMemo memo, int bus) {
+        _bus = bus;
+        _memo = memo;
+    }
+
+    @Override
+    public String getSystemPrefix() {
+        return _memo.getSystemPrefix();
+    }
+
+    @Override
+    public Turnout createNewTurnout(String systemName, String userName) {
+        Turnout t;
+        int addr = Integer.parseInt(systemName.substring(_memo.getSystemPrefix().length() + 1));
+        t = new SRCPTurnout(addr, _memo);
+        t.setUserName(userName);
+
+        return t;
+    }
+
+    @Override
+    public boolean allowMultipleAdditions(String systemName) {
+        return true;
+    }
+
+    // private final static Logger log = LoggerFactory.getLogger(SRCPTurnout.class);
+
+}
