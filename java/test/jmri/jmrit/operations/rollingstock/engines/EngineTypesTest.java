6f8512553b14e45cd3adea44d90660b3fb6e464d
Luc Esape <luc.esape@gmail.com>
2018-10-02 20:14:28 +0200

    Bug commit from the following repository JMRI/JMRI This bug commit is a reflect of source code from: http://github.com/JMRI/JMRI/commit/cab122514d311493200a8a0db1d003844c2774c4.

---
+++ java/test/jmri/jmrit/operations/rollingstock/engines/EngineTypesTest.java
@@ -0,0 +1,79 @@
+package jmri.jmrit.operations.rollingstock.engines;
+
+import jmri.jmrit.operations.OperationsTestCase;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for the Operations RollingStock Engine class Last manually
+ * cross-checked on 20090131
+ *
+ * Still to do: Engine: Destination Engine: Verify everything else EngineTypes:
+ * get/set Names lists EngineModels: get/set Names lists EngineLengths:
+ * Everything Consist: Everything Import: Everything EngineManager: Engine
+ * register/deregister EngineManager: Consists
+ *
+ * @author	Bob Coleman Copyright (C) 2008, 2009
+ */
+public class EngineTypesTest extends OperationsTestCase {
+
+    // test EngineTypes Class
+    // test EngineTypes creation
+    @Test
+    public void testEngineTypesCreate() {
+        EngineTypes et1 = new EngineTypes();
+        Assert.assertNotNull("exists", et1);
+    }
+
+    // test EngineTypes public constants
+    @Test
+    public void testEngineTypesConstants() {
+        EngineTypes et1 = new EngineTypes();
+
+        Assert.assertNotNull("exists", et1);
+        Assert.assertEquals("EngineTypes ENGINETYPES_LENGTH_CHANGED_PROPERTY", "EngineTypesLength", EngineTypes.ENGINETYPES_CHANGED_PROPERTY);
+        Assert.assertEquals("EngineTypes ENGINETYPES_NAME_CHANGED_PROPERTY", "EngineTypesName", EngineTypes.ENGINETYPES_NAME_CHANGED_PROPERTY);
+    }
+
+    // test EngineTypes Names
+    @Test
+    public void testEngineTypesNames() {
+        EngineTypes et1 = new EngineTypes();
+
+        Assert.assertEquals("EngineTypes Null Names", false, et1.containsName("TESTENGINETYPENAME1"));
+
+        et1.addName("TESTENGINETYPENAME1");
+        Assert.assertEquals("EngineTypes add Name1", true, et1.containsName("TESTENGINETYPENAME1"));
+
+        et1.addName("TESTENGINETYPENAME2");
+        Assert.assertEquals("EngineTypes add Name2", true, et1.containsName("TESTENGINETYPENAME2"));
+
+        et1.replaceName("TESTENGINETYPENAME1", "TESTENGINETYPENAME3");
+        Assert.assertEquals("EngineTypes replace Name1", false, et1.containsName("TESTENGINETYPENAME1"));
+        Assert.assertEquals("EngineTypes replace Name3", true, et1.containsName("TESTENGINETYPENAME3"));
+
+        et1.deleteName("TESTENGINETYPENAME2");
+        Assert.assertEquals("EngineTypes delete Name2", false, et1.containsName("TESTENGINETYPENAME2"));
+
+        et1.deleteName("TESTENGINETYPENAME3");
+        Assert.assertEquals("EngineTypes delete Name3", false, et1.containsName("TESTENGINETYPENAME3"));
+    }
+
+    // from here down is testing infrastructure
+    // Ensure minimal setup for log4J
+    @Override
+    @Before
+    public void setUp() {
+        super.setUp();
+    }
+
+    @Override
+    @After
+    public void tearDown() {
+       super.tearDown();
+    }
+
+
+}
