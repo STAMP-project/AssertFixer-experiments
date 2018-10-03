edd584da4437d4db06c3067a383c00eb6dbaff79
Luc Esape <luc.esape@gmail.com>
2018-10-03 07:39:56 +0200

    Bug commit from the following repository bobjacobsen/JMRI This bug commit is a reflect of source code from: http://github.com/bobjacobsen/JMRI/commit/73ac4187709e454f64fc568e001f2099797396f1.

---
+++ java/test/jmri/jmrit/operations/rollingstock/engines/EngineLengthsTest.java
@@ -0,0 +1,73 @@
+package jmri.jmrit.operations.rollingstock.engines;
+
+import jmri.jmrit.operations.OperationsTestCase;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for the Operations RollingStock Engine class Last manually
+ * cross-checked on 20090131
+ *
+ * Still to do: Engine: Destination Engine: Verify everything else EngineTypes:
+ * get/set Names lists EngineModels: get/set Names lists EngineLengths:
+ * Everything Consist: Everything Import: Everything EngineManager: Engine
+ * register/deregister EngineManager: Consists
+ *
+ * @author	Bob Coleman Copyright (C) 2008, 2009
+ */
+public class EngineLengthsTest extends OperationsTestCase {
+
+    // test EngineLengths Class
+    // test EngineLengths creation
+    @Test
+    public void testEngineLengthsCreate() {
+        EngineLengths el1 = new EngineLengths();
+        Assert.assertNotNull("exists", el1);
+    }
+
+    // test EngineLengths public constants
+    @Test
+    public void testEngineLengthsConstants() {
+        EngineLengths el1 = new EngineLengths();
+
+        Assert.assertNotNull("exists", el1);
+        Assert.assertEquals("EngineTypes ENGINELENGTHS_CHANGED_PROPERTY", "EngineLengths", EngineLengths.ENGINELENGTHS_CHANGED_PROPERTY);
+    }
+
+    // test EngineLengths Names
+    @Test
+    public void testEngineLengthsNames() {
+        EngineLengths el1 = new EngineLengths();
+
+        Assert.assertEquals("EngineLengths Null Names", false, el1.containsName("TESTENGINELENGTHNAME1"));
+
+        el1.addName("TESTENGINELENGTHNAME1");
+        Assert.assertEquals("EngineLengths add Name1", true, el1.containsName("TESTENGINELENGTHNAME1"));
+
+        el1.addName("TESTENGINELENGTHNAME2");
+        Assert.assertEquals("EngineLengths add Name2", true, el1.containsName("TESTENGINELENGTHNAME2"));
+
+        el1.deleteName("TESTENGINELENGTHNAME2");
+        Assert.assertEquals("EngineLengths delete Name2", false, el1.containsName("TESTENGINELENGTHNAME2"));
+
+        el1.deleteName("TESTENGINELENGTHNAME1");
+        Assert.assertEquals("EngineLengths delete Name1", false, el1.containsName("TESTENGINELENGTHNAME1"));
+    }
+
+    // TODO: Add test for import
+    // from here down is testing infrastructure
+    // Ensure minimal setup for log4J
+    @Override
+    @Before
+    public void setUp() {
+        super.setUp();
+    }
+
+    @Override
+    @After
+    public void tearDown() {
+       super.tearDown();
+    }
+}
