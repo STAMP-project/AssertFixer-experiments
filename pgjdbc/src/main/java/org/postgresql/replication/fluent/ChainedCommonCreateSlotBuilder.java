02a2ec9b7afda141248d1524469a04acd51f433d
Luc Esape <luc.esape@gmail.com>
2018-10-04 01:06:00 +0200

    Bug commit from the following repository pgjdbc/pgjdbc This bug commit is a reflect of source code from: http://github.com/pgjdbc/pgjdbc/commit/1e638bd8b2b68058a597ad1c372cba9c56c5e210.

---
+++ pgjdbc/src/main/java/org/postgresql/replication/fluent/ChainedCommonCreateSlotBuilder.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016, PostgreSQL Global Development Group
+ * See the LICENSE file in the project root for more information.
+ */
+
+package org.postgresql.replication.fluent;
+
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+
+/**
+ * Fluent interface for specify common parameters for create Logical and Physical replication slot.
+ */
+public interface ChainedCommonCreateSlotBuilder<T extends ChainedCommonCreateSlotBuilder<T>>  {
+
+  /**
+   * Replication slots provide an automated way to ensure that the master does not remove WAL
+   * segments until they have been received by all standbys, and that the master does not remove
+   * rows which could cause a recovery conflict even when the standby is disconnected.
+   *
+   * @param slotName not null unique replication slot name for create.
+   * @return T a slot builder
+   */
+  T withSlotName(String slotName);
+
+  /**
+   * <p>Temporary slots are not saved to disk and are automatically dropped on error or when
+   * the session has finished.</p>
+   *
+   * <p>This feature is only supported by PostgreSQL versions &gt;= 10.</p>
+   *
+   * @param temporaryOption specifies if the replication slot should be a temporary one.
+   * @return T a slot builder
+   * @throws SQLFeatureNotSupportedException thrown if PostgreSQL version is less than 10.
+   */
+  T withTemporaryOption(boolean temporaryOption) throws SQLFeatureNotSupportedException;
+
+  /**
+   * Create slot with specified parameters in database.
+   * @throws SQLException on error
+   */
+  void make() throws SQLException;
+}
---
+++ pgjdbc/src/main/java/org/postgresql/replication/fluent/logical/LogicalCreateSlotBuilder.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2016, PostgreSQL Global Development Group
+ * See the LICENSE file in the project root for more information.
+ */
+
+package org.postgresql.replication.fluent.logical;
+
+import org.postgresql.core.BaseConnection;
+import org.postgresql.replication.fluent.AbstractCreateSlotBuilder;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class LogicalCreateSlotBuilder
+    extends AbstractCreateSlotBuilder<ChainedLogicalCreateSlotBuilder>
+    implements ChainedLogicalCreateSlotBuilder {
+  private String outputPlugin;
+
+  public LogicalCreateSlotBuilder(BaseConnection connection) {
+    super(connection);
+  }
+
+  @Override
+  protected ChainedLogicalCreateSlotBuilder self() {
+    return this;
+  }
+
+  @Override
+  public ChainedLogicalCreateSlotBuilder withOutputPlugin(String outputPlugin) {
+    this.outputPlugin = outputPlugin;
+    return self();
+  }
+
+  @Override
+  public void make() throws SQLException {
+    if (outputPlugin == null || outputPlugin.isEmpty()) {
+      throw new IllegalArgumentException(
+          "OutputPlugin required parameter for logical replication slot");
+    }
+
+    if (slotName == null || slotName.isEmpty()) {
+      throw new IllegalArgumentException("Replication slotName can't be null");
+    }
+
+    Statement statement = connection.createStatement();
+    try {
+      statement.execute(String.format(
+              "CREATE_REPLICATION_SLOT %s %s LOGICAL %s",
+              slotName,
+              temporaryOption ? "TEMPORARY" : "",
+              outputPlugin
+      ));
+    } finally {
+      statement.close();
+    }
+  }
+}
