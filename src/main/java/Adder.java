52c9466997749a0957773bb5ead0f765c0f47542
Luc Esape <luc.esape@gmail.com>
2018-10-06 07:57:19 +0200

    Bug commit from the following repository catherinelee95/SimpleCalc This bug commit is a reflect of source code from: http://github.com/catherinelee95/SimpleCalc/commit/f62b7d0ee3eec364b71039f2ec4a7c59a6089a01.

---
+++ src/main/java/Adder.java
@@ -0,0 +1,18 @@
+
+public class Adder {
+	public int x = 5;
+	public int y = 5;
+	
+	public int getDefaultSum() {
+		return x+y;
+	}
+	
+	public int getSum(int a, int b) {
+		return a+b;
+	}
+	
+	public int getQuotient(int a, int b) {
+		return a/b;
+	}
+
+}
---
+++ src/test/java/AdderTest.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AdderTest {
+	
+	@Test
+	public void testDefault() {
+		Adder adder = new Adder();
+		int sum = adder.getDefaultSum();
+		assertEquals(sum, 10);
+	}
+	
+	@Test
+	public void testSum() {
+		int a = 3;
+		int b = 4;
+		
+		Adder adder = new Adder();
+		int sum = adder.getSum(a,  b);
+		assertEquals(sum, 7);
+	}
+	
+	@Test
+	public void testQuotient() {
+		int a = 4;
+		int b = 0;
+		
+		Adder adder = new Adder();
+		int sum = adder.getQuotient(a, b);
+		assertEquals(sum, 2);
+	}
+	
+	
+	
+}
