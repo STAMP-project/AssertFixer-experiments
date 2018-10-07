ca3b1c54081cd81075b4c2b28a4fd0faac2e042b
Luc Esape <luc.esape@gmail.com>
2018-10-07 05:10:19 +0200

    Bug commit from the following repository pabender/JMRI This bug commit is a reflect of source code from: http://github.com/pabender/JMRI/commit/8197913f81dd08e0464d962fb6adfd3050d13d41.

---
+++ java/src/jmri/configurexml/AbstractXmlAdapter.java
@@ -0,0 +1,89 @@
+package jmri.configurexml;
+
+import org.jdom2.Element;
+
+/**
+ * Abstract class to provide basic error handling for XmlAdapter
+ *
+ * @author Bob Jacobsen Copyright (c) 2009
+ * @see XmlAdapter
+ */
+public abstract class AbstractXmlAdapter implements XmlAdapter {
+
+    private ErrorHandler errorHandler = XmlAdapter.getDefaultExceptionHandler();
+
+    /** {@inheritDoc} */
+    @Override
+    public void handleException(
+            String description,
+            String operation,
+            String systemName,
+            String userName,
+            Exception exception) {
+        if (errorHandler != null) {
+            this.errorHandler.handle(new ErrorMemo(this, operation, description, systemName, userName, exception));
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean load(Element e) throws JmriConfigureXmlException {
+        throw new UnsupportedOperationException("One of the other load methods must be implemented.");
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean load(Element shared, Element perNode) throws JmriConfigureXmlException { // may not need exception
+        return this.load(shared);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void load(Element shared, Element perNode, Object o) throws JmriConfigureXmlException { // may not need exception
+        this.load(shared, o);
+    }
+
+    /**
+     * Determine if this set of configured objects should be loaded after basic
+     * GUI construction is completed.
+     * <p>
+     * Default behavior is to load when requested. Classes that should wait
+     * until basic GUI is constructed should override this method and return
+     * true
+     *
+     * @return true to defer loading
+     * @see jmri.configurexml.XmlAdapter#loadDeferred()
+     * @since 2.11.2
+     */
+    @Override
+    public boolean loadDeferred() {
+        return false;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int loadOrder() {
+        return 50;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Element store(Object o, boolean shared) {
+        if (shared) {
+            return this.store(o);
+        }
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void setExceptionHandler(ErrorHandler errorHandler) {
+        this.errorHandler = errorHandler;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public ErrorHandler getExceptionHandler() {
+        return this.errorHandler;
+    }
+}
---
+++ java/test/jmri/jmrit/operations/locations/LocationEditFrameTest.java
@@ -0,0 +1,80 @@
+package jmri.jmrit.operations.locations;
+
+import java.awt.GraphicsEnvironment;
+import jmri.InstanceManager;
+import jmri.jmrit.operations.OperationsTestCase;
+import jmri.util.JUnitOperationsUtil;
+import jmri.util.JUnitUtil;
+import jmri.util.swing.JemmyUtil;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for the Operations Locations GUI class
+ *
+ * @author	Dan Boudreau Copyright (C) 2009
+ */
+public class LocationEditFrameTest extends OperationsTestCase {
+
+    final static int ALL = Track.EAST + Track.WEST + Track.NORTH + Track.SOUTH;
+
+    @Test
+    public void testLocationEditFrame() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        JUnitOperationsUtil.loadFiveLocations();
+
+        LocationEditFrame f = new LocationEditFrame(null);
+        f.setTitle("Test Add Location Frame");
+
+        f.locationNameTextField.setText("New Test Location");
+        JemmyUtil.enterClickAndLeave(f.addLocationButton);
+
+        LocationManager lManager = InstanceManager.getDefault(LocationManager.class);
+        Assert.assertEquals("should be 6 locations", 6, lManager.getLocationsByNameList().size());
+        Location newLoc = lManager.getLocationByName("New Test Location");
+
+        Assert.assertNotNull(newLoc);
+
+        // add a yard track
+        JemmyUtil.enterClickAndLeave(f.addYardButton);
+
+        // add an interchange track
+        JemmyUtil.enterClickAndLeave(f.addInterchangeButton);
+
+        // add a staging track
+        JemmyUtil.enterClickAndLeave(f.addStagingButton);
+
+        // add a yard track
+        JemmyUtil.enterClickAndLeave(f.addYardButton);
+
+        f.locationNameTextField.setText("Newer Test Location");
+        JemmyUtil.enterClickAndLeave(f.saveLocationButton);
+
+        Assert.assertEquals("changed location name", "Newer Test Location", newLoc.getName());
+
+        // test delete button
+        JemmyUtil.enterClickAndLeave(f.deleteLocationButton);
+        Assert.assertEquals("should be 6 locations", 6, lManager.getLocationsByNameList().size());
+        // confirm delete dialog window should appear
+        JemmyUtil.pressDialogButton(f, Bundle.getMessage("deletelocation?"), Bundle.getMessage("ButtonYes"));
+        // location now deleted
+        Assert.assertEquals("should be 5 locations", 5, lManager.getLocationsByNameList().size());
+
+        JUnitUtil.dispose(f);
+    }
+
+    @Override
+    @Before
+    public void setUp() {
+        super.setUp();
+    }
+
+    @Override
+    @After
+    public void tearDown() {
+        super.tearDown();
+    }
+}
