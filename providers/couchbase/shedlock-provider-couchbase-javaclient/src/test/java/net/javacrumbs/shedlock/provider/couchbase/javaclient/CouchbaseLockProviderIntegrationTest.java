79c81a146834fae5802911888bc46567ccaa2adc
Luc Esape <luc.esape@gmail.com>
2018-10-07 21:19:36 +0200

    Bug commit from the following repository lukas-krecan/ShedLock This bug commit is a reflect of source code from: http://github.com/lukas-krecan/ShedLock/commit/bf1d800469ffa44d68cbdd160ce7465572a70b07.

---
+++ providers/couchbase/shedlock-provider-couchbase-javaclient/src/test/java/net/javacrumbs/shedlock/provider/couchbase/javaclient/CouchbaseLockProviderIntegrationTest.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright 2009-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.javacrumbs.shedlock.provider.couchbase.javaclient;
+
+
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.CouchbaseCluster;
+import com.couchbase.client.java.document.JsonDocument;
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.test.support.AbstractLockProviderIntegrationTest;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import static java.time.Instant.now;
+import static java.time.Instant.parse;
+import static net.javacrumbs.shedlock.provider.couchbase.javaclient.CouchbaseLockProvider.LOCKED_AT;
+import static net.javacrumbs.shedlock.provider.couchbase.javaclient.CouchbaseLockProvider.LOCKED_BY;
+import static net.javacrumbs.shedlock.provider.couchbase.javaclient.CouchbaseLockProvider.LOCK_UNTIL;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CouchbaseLockProviderIntegrationTest extends AbstractLockProviderIntegrationTest{
+
+    private static final String BUCKET_NAME = "test";
+    private static final String HOST = "127.0.0.1";
+
+
+    private CouchbaseLockProvider lockProvider;
+    private static Cluster cluster;
+    private static Bucket bucket;
+
+    @BeforeClass
+    public static void startCouchbase () {
+        cluster = connect();
+        cluster.authenticate("Administrator", "password");
+
+        bucket = cluster.openBucket(BUCKET_NAME);
+    }
+
+    @AfterClass
+    public static void stopCouchbase () {
+        disconnect(cluster);
+    }
+
+    @Before
+    public void createLockProvider()  {
+        bucket = getBucket(cluster);
+        lockProvider = new CouchbaseLockProvider(bucket);
+    }
+
+    @After
+    public void clear() {
+        try {
+            bucket.remove(LOCK_NAME1);
+        } catch (Exception e) {
+            // ignore
+        }
+    }
+
+    @Override
+    protected LockProvider getLockProvider() {
+        return lockProvider;
+    }
+
+    @Override
+    public void assertUnlocked(String lockName) {
+        JsonDocument lockDocument = bucket.get(lockName);
+        assertThat(parse((String) lockDocument.content().get(LOCK_UNTIL))).isBefore(now());
+        assertThat(parse((String) lockDocument.content().get(LOCKED_AT))).isBefore(now());
+        assertThat(lockDocument.content().get(LOCKED_BY)).asString().isNotEmpty();
+    }
+
+    @Override
+    public void assertLocked(String lockName) {
+        JsonDocument lockDocument = bucket.get(lockName);
+        assertThat(parse((String) lockDocument.content().get(LOCK_UNTIL))).isAfter(now());
+        assertThat(parse((String) lockDocument.content().get(LOCKED_AT))).isBefore(now());
+        assertThat(lockDocument.content().get(LOCKED_BY)).asString().isNotEmpty();
+    }
+
+    private static Cluster connect(){
+        return CouchbaseCluster.create(HOST);
+    }
+
+    private static void disconnect(Cluster cluster){
+        cluster.disconnect();
+    }
+
+    private Bucket getBucket(Cluster cluster) {
+        return bucket;
+    }
+
+}
---
+++ shedlock-test-support/src/main/java/net/javacrumbs/shedlock/test/support/AbstractLockProviderIntegrationTest.java
@@ -0,0 +1,146 @@
+/**
+ * Copyright 2009-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.javacrumbs.shedlock.test.support;
+
+import net.javacrumbs.shedlock.core.LockConfiguration;
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.core.SimpleLock;
+import org.junit.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
+import static java.lang.Thread.sleep;
+import static java.time.temporal.ChronoUnit.MINUTES;
+import static java.time.temporal.ChronoUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public abstract class AbstractLockProviderIntegrationTest {
+    protected static final String LOCK_NAME1 = "name";
+    public static final Duration LOCK_AT_LEAST_FOR = Duration.of(2, SECONDS);
+
+    protected abstract LockProvider getLockProvider();
+
+    protected abstract void assertUnlocked(String lockName);
+
+    protected abstract void assertLocked(String lockName);
+
+
+    @Test
+    public void shouldCreateLock() {
+        Optional<SimpleLock> lock = getLockProvider().lock(lockConfig(LOCK_NAME1));
+        assertThat(lock).isNotEmpty();
+
+        assertLocked(LOCK_NAME1);
+        lock.get().unlock();
+        assertUnlocked(LOCK_NAME1);
+
+    }
+
+    @Test
+    public void shouldNotReturnSecondLock() {
+        Optional<SimpleLock> lock = getLockProvider().lock(lockConfig(LOCK_NAME1));
+        assertThat(lock).isNotEmpty();
+        assertThat(getLockProvider().lock(lockConfig(LOCK_NAME1))).isEmpty();
+        lock.get().unlock();
+    }
+
+    @Test
+    public void shouldCreateTwoIndependentLocks() {
+        Optional<SimpleLock> lock1 = getLockProvider().lock(lockConfig(LOCK_NAME1));
+        assertThat(lock1).isNotEmpty();
+
+        Optional<SimpleLock> lock2 = getLockProvider().lock(lockConfig("name2"));
+        assertThat(lock2).isNotEmpty();
+
+        lock1.get().unlock();
+        lock2.get().unlock();
+    }
+
+    @Test
+    public void shouldLockTwiceInARow() {
+        Optional<SimpleLock> lock1 = getLockProvider().lock(lockConfig(LOCK_NAME1));
+        assertThat(lock1).isNotEmpty();
+        lock1.get().unlock();
+
+        Optional<SimpleLock> lock2 = getLockProvider().lock(lockConfig(LOCK_NAME1));
+        assertThat(lock2).isNotEmpty();
+        lock2.get().unlock();
+    }
+
+    @Test
+    public void shouldTimeout() throws InterruptedException {
+        LockConfiguration configWithShortTimeout = lockConfig(LOCK_NAME1, Duration.ofMillis(20), Duration.ZERO);
+        Optional<SimpleLock> lock1 = getLockProvider().lock(configWithShortTimeout);
+        assertThat(lock1).isNotEmpty();
+
+        sleep(25);
+        assertUnlocked(LOCK_NAME1);
+
+        Optional<SimpleLock> lock2 = getLockProvider().lock(lockConfig(LOCK_NAME1, Duration.ofMillis(5), Duration.ZERO));
+        assertThat(lock2).isNotEmpty();
+        lock2.get().unlock();
+    }
+
+
+    @Test
+    public void shouldBeAbleToLockRightAfterUnlock() {
+        LockConfiguration lockConfiguration = lockConfig(LOCK_NAME1);
+        LockProvider lockProvider = getLockProvider();
+        for (int i = 0; i < 100; i++) {
+            Optional<SimpleLock> lock = lockProvider.lock(lockConfiguration);
+            assertThat(lockProvider.lock(lockConfiguration)).isEmpty();
+            assertThat(lock).isNotEmpty();
+            lock.get().unlock();
+        }
+    }
+
+    @Test
+    public void fuzzTestShouldPass() throws ExecutionException, InterruptedException {
+        new FuzzTester(getLockProvider()).doFuzzTest();
+    }
+
+    @Test
+    public void shouldLockAtLeastFor() throws InterruptedException {
+        // Lock for LOCK_AT_LEAST_FOR - we do not expect the lock to be released before this time
+        Optional<SimpleLock> lock1 = getLockProvider().lock(lockConfig(LOCK_NAME1, LOCK_AT_LEAST_FOR.multipliedBy(2), LOCK_AT_LEAST_FOR));
+        assertThat(lock1).isNotEmpty();
+        lock1.get().unlock();
+
+        // Even though we have unlocked the lock, it will be held for some time
+        assertThat(getLockProvider().lock(lockConfig(LOCK_NAME1))).describedAs("Can not acquire lock, grace period did not pass yet").isEmpty();
+
+        // Let's wait wor the lock to be automatically released
+        sleep(LOCK_AT_LEAST_FOR.toMillis());
+
+        // Should be able to acquire now
+        Optional<SimpleLock> lock3 = getLockProvider().lock(lockConfig(LOCK_NAME1));
+        assertThat(lock3).describedAs("Can acquire the lock after grace period").isNotEmpty();
+        lock3.get().unlock();
+    }
+
+    protected static LockConfiguration lockConfig(String name) {
+        return lockConfig(name, Duration.of(5, MINUTES), Duration.ZERO);
+    }
+
+    protected static LockConfiguration lockConfig(String name, Duration lockAtMostFor, Duration lockAtLeastFor) {
+        Instant now = Instant.now();
+        return new LockConfiguration(name, now.plus(lockAtMostFor), now.plus(lockAtLeastFor));
+    }
+
+}
