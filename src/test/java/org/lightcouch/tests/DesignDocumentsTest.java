9b7d1a782c0a55256c248d32f67128cf463f3c90
Luc Esape <luc.esape@gmail.com>
2018-10-08 17:30:46 +0200

    Bug commit from the following repository IndabaConsultores/LightCouch This bug commit is a reflect of source code from: http://github.com/IndabaConsultores/LightCouch/commit/8be5df0bd1f3d924b94376f87f5ba2c93de13049.

---
+++ src/test/java/org/lightcouch/tests/DesignDocumentsTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2011 indaba.es
+ * Copyright (C) 2011 lightcouch.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lightcouch.tests;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.lightcouch.DesignDocument;
+
+public class DesignDocumentsTest extends CouchDbTestBase {
+
+	@Test
+	public void designDocSync() {
+		DesignDocument designDoc = dbClient.design().getFromDesk("example");
+		dbClient.design().synchronizeWithDb(designDoc);
+	}
+	
+	@Test
+	public void designDocCompare() {
+		DesignDocument designDoc1 = dbClient.design().getFromDesk("example");
+		dbClient.design().synchronizeWithDb(designDoc1);
+		
+		DesignDocument designDoc11 = dbClient.design().getFromDb("_design/example");
+		
+		assertEquals(designDoc1, designDoc11);
+	}
+	
+	@Test
+	public void designDocs() {
+		List<DesignDocument> designDocs = dbClient.design().getAllFromDesk();
+		dbClient.syncDesignDocsWithDb();
+		
+		assertThat(designDocs.size(), not(0));
+	}
+	
+	@Test
+    public void containsDesignDoc() {
+	    
+	    assertFalse(dbClient.design().contains("_design/test-design-doc-unknown"));
+	    
+	    DesignDocument designDoc1 = dbClient.design().getFromDesk("test-design-doc");
+        dbClient.design().synchronizeWithDb(designDoc1);
+        
+        assertTrue(dbClient.design().contains("_design/test-design-doc"));
+	    
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+    public void containsDesignDocIncorrectId() {
+	    dbClient.design().contains("test-design-doc");
+	}
+
+}
