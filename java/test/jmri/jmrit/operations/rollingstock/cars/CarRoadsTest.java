f1ceefafddd14d89ad8f54c1b44ac42b8898d87d
Luc Esape <luc.esape@gmail.com>
2018-10-08 03:50:23 +0200

    Bug commit from the following repository JMRI/JMRI This bug commit is a reflect of source code from: http://github.com/JMRI/JMRI/commit/65d4ff56caa931e9e66e4e215ae4e11a5e3e348f.

---
+++ java/test/jmri/jmrit/operations/rollingstock/cars/CarRoadsTest.java
@@ -0,0 +1,69 @@
+package jmri.jmrit.operations.rollingstock.cars;
+
+import javax.swing.JComboBox;
+import jmri.InstanceManager;
+import jmri.jmrit.operations.OperationsTestCase;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for the Operations RollingStock Cars Roads class Last manually
+ * cross-checked on 20090131
+ * <p>
+ * Still to do: Everything
+ *
+ * @author	Bob Coleman Copyright (C) 2008, 2009
+ */
+public class CarRoadsTest extends OperationsTestCase {
+
+    @Test
+    public void testDefaultCarRoads() {
+        CarRoads cr1 = InstanceManager.getDefault(CarRoads.class);
+
+        // the previous version of this test looked for specific values,
+        // but those specific road names may not exist in a non-US 
+        // English context. 
+        Assert.assertNotNull("Car Roads not empty", cr1.getNames());
+    }
+
+    @Test
+    public void testAddAndDeleteCarRoads() {
+        CarRoads cr1 = InstanceManager.getDefault(CarRoads.class);
+        cr1.getNames();	//load predefined roads
+        cr1.addName("Road New1");
+        Assert.assertTrue("Car Roads Add New1", cr1.containsName("Road New1"));
+        Assert.assertFalse("Car Roads Never Added New2", cr1.containsName("Road New2"));
+        cr1.addName("Road New3");
+        Assert.assertTrue("Car Roads Still Has New1", cr1.containsName("Road New1"));
+        Assert.assertTrue("Car Roads Add New3", cr1.containsName("Road New3"));
+        cr1.replaceName("Road New3", "Road New4");
+        Assert.assertFalse("Car Roads replace New3", cr1.containsName("Road New3"));
+        Assert.assertTrue("Car Roads replace New3 with New4", cr1.containsName("Road New4"));
+        String[] roads = cr1.getNames();
+        Assert.assertEquals("First road name", "Road New4", roads[0]);
+        Assert.assertEquals("2nd road name", "Road New1", roads[1]);
+        JComboBox<?> box = cr1.getComboBox();
+        Assert.assertEquals("First comboBox road name", "Road New4", box.getItemAt(0));
+        Assert.assertEquals("2nd comboBox road name", "Road New1", box.getItemAt(1));
+        cr1.deleteName("Road New4");
+        Assert.assertFalse("Car Roads Delete New4", cr1.containsName("Road New4"));
+        cr1.deleteName("Road New1");
+        Assert.assertFalse("Car Roads Delete New1", cr1.containsName("Road New1"));
+    }
+
+    // from here down is testing infrastructure
+    // Ensure minimal setup for log4J
+    @Override
+    @Before
+    public void setUp() {
+        super.setUp();
+    }
+
+    @Override
+    @After
+    public void tearDown() {
+        super.tearDown();
+    }
+}
---
+++ java/test/jmri/util/ThreadingUtilTest.java
@@ -0,0 +1,172 @@
+package jmri.util;
+
+import org.junit.*;
+
+/**
+ * Tests for ThreadingUtil class
+ *
+ * @author	Bob Jacobsen Copyright 2015
+ */
+public class ThreadingUtilTest {
+
+    boolean done;
+    
+    @Test
+    public void testToLayout() {
+        done = false;
+        
+        ThreadingUtil.runOnLayout( ()-> { 
+            done = true; 
+        } );
+        
+        Assert.assertTrue(done);
+    }
+
+    Object testRef = null;
+    @Test
+    public void testToGuiWarn() {
+        // if (!java.lang.management.ManagementFactory
+        //                                        .getThreadMXBean().isObjectMonitorUsageSupported())
+        //        log.info("This JVM doesn't support object monitor tracking");
+        // if (!java.lang.management.ManagementFactory
+        //                                        .getThreadMXBean().isSynchronizerUsageSupported())
+        //        log.info("This JVM doesn't support synchronized lock tracking");
+                                                
+        final Object lockedThing = new Object();
+        done = false;
+        
+        synchronized (lockedThing) {
+            // first, run something that also wants the lock
+            new Thread(() -> {
+                synchronized (lockedThing) {
+                    testRef = lockedThing;
+                }
+            }).start();
+            
+            ThreadingUtil.runOnGUI( ()-> { 
+                done = true; 
+                Assert.assertNull(testRef); // due to lock
+            } );
+ 
+            JUnitUtil.waitFor( ()->{ return done; }, "GUI thread complete");
+            Assert.assertNull(testRef); // due to lock
+        }
+        JUnitUtil.waitFor( ()->{ return testRef != null; }, "Locked thread complete");
+    }
+
+    @Test
+    public void testThreadingNesting() {
+        done = false;
+        
+        Thread t = new Thread(
+            new Runnable() {
+                @Override
+                public void run() {
+                    // now on another thread
+                    // switch back to Layout thread
+                    ThreadingUtil.runOnLayout( ()-> { 
+                        // on layout thread, confirm
+                        Assert.assertTrue("on Layout thread", ThreadingUtil.isLayoutThread());
+                        // mark done so we know
+                        done = true; 
+                    } );
+                }
+            }
+        );
+        t.setName("Thread Nesting Test Thread");
+        t.start();
+
+        // wait for separate thread to do it's work before confirming test
+        JUnitUtil.waitFor( ()->{ return done; }, "Separate thread complete");
+    }
+
+    @Test
+    public void testThreadingNestingToSwing() {
+        done = false;
+        
+        javax.swing.SwingUtilities.invokeLater(
+            new Runnable() {
+                @Override
+                public void run() {
+                    // now on Swing thread
+                    // switch back to Layout thread
+                    ThreadingUtil.runOnLayout( ()-> { 
+                        // on layout thread, confirm
+                        Assert.assertTrue("on Layout thread", ThreadingUtil.isLayoutThread());
+                        // mark done so we known
+                        done = true; 
+                    } );
+                }
+            }
+        );
+
+        // wait for separate thread to do it's work before confirming test
+        JUnitUtil.waitFor( ()->{ return done; }, "Separate thread complete");
+    }
+
+    @Test
+    public void testThreadingDelayGUI() {
+        done = false;
+        
+        ThreadingUtil.runOnGUIDelayed( ()-> { 
+            done = true; 
+        }, 200 );
+
+        // ensure not done now
+        Assert.assertTrue(!done);
+        
+        // wait for separate thread to do it's work before confirming test
+        JUnitUtil.waitFor( ()->{ return done; }, "Delayed operation complete");
+    }
+
+    @Test
+    public void testThreadingRunOnGUIwithReturn() {
+        done = false;
+        
+        Integer value = ThreadingUtil.runOnGUIwithReturn( ()-> { 
+            done = true; 
+            return new Integer(21);
+        });
+
+        Assert.assertTrue(done);
+        Assert.assertEquals(new Integer(21), value);
+    }
+
+    @Test
+    public void testThreadingDelayLayout() {
+        done = false;
+        
+        ThreadingUtil.runOnLayoutDelayed( ()-> { 
+            done = true; 
+        }, 200 );
+
+        // ensure not done now
+        Assert.assertTrue(!done);
+        
+        // wait for separate thread to do it's work before confirming test
+        JUnitUtil.waitFor( ()->{ return done; }, "Delayed oepration complete");
+    }
+
+    /**
+     * Show how to query state of _current_ thread
+     */
+    @Test
+    public void testSelfState() {
+    
+        // To run the tests, this thread has to be running, not waiting
+        Assert.assertTrue(ThreadingUtil.canThreadRun(Thread.currentThread()));
+        Assert.assertFalse(ThreadingUtil.isThreadWaiting(Thread.currentThread()));
+    }
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() throws Exception {
+        jmri.util.JUnitUtil.setUp();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        jmri.util.JUnitUtil.tearDown();
+    }
+
+}
