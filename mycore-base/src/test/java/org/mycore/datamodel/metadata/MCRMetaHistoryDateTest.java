b475334f7ed7e4b044efeae548db18a844e99cb4
Luc Esape <luc.esape@gmail.com>
2018-10-09 14:29:22 +0200

    Bug commit from the following repository MyCoRe-Org/mycore This bug commit is a reflect of source code from: http://github.com/MyCoRe-Org/mycore/commit/b5192ed16052e2f85e8e6b14975564a93306f9bd.

---
+++ mycore-base/src/test/java/org/mycore/datamodel/metadata/MCRMetaHistoryDateTest.java
@@ -0,0 +1,95 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mycore.datamodel.metadata;
+
+import static org.junit.Assert.*;
+
+import org.jdom2.Element;
+import org.junit.Test;
+import org.mycore.common.MCRCalendar;
+import org.mycore.common.MCRTestCase;
+import org.mycore.common.xml.MCRXMLHelper;
+
+import com.ibm.icu.util.GregorianCalendar;
+
+/**
+ * This class is a JUnit test case for org.mycore.datamodel.metadata.MCRMetaHistoryDate.
+ * 
+ * @author Jens Kupferschmidt
+ * @version $Revision$ $Date$
+ *
+ */
+public class MCRMetaHistoryDateTest extends MCRTestCase {
+    
+    /**
+     * check set date methods
+     */
+    @Test
+    public void checkSetDateMethods() {
+        MCRMetaHistoryDate hd = new MCRMetaHistoryDate();
+        hd.setVonDate(new GregorianCalendar(1964, 1, 23));
+        assertEquals("Von value is not 1964-02-23 AD", "1964-02-23 AD", hd.getVonToString());
+
+        hd = new MCRMetaHistoryDate();
+        hd.setVonDate("23.02.1964", MCRCalendar.TAG_GREGORIAN);
+        assertEquals("Von value is not 1964-02-23 AD", "1964-02-23 AD", hd.getVonToString());
+
+        hd = new MCRMetaHistoryDate();
+        hd.setBisDate(new GregorianCalendar(1964, 1, 23));
+        assertEquals("Bis value is not 1964-02-23 AD", "1964-02-23 AD", hd.getBisToString());
+
+        hd = new MCRMetaHistoryDate();
+        hd.setBisDate("23.02.1964", MCRCalendar.TAG_GREGORIAN);
+        assertEquals("Bis value is not 1964-02-23 AD", "1964-02-23 AD", hd.getBisToString());
+    }
+
+    /**
+     * check createXML, setFromDom, equals and clone
+     */
+    @Test
+    public void checkCreateParseEqualsClone() {
+        
+        MCRMetaHistoryDate julian_date = new MCRMetaHistoryDate("subtag","type",0);
+        julian_date.setCalendar(MCRCalendar.TAG_JULIAN);
+        julian_date.setVonDate("22.02.1964", julian_date.getCalendar());
+        julian_date.setBisDate("22.02.1964", julian_date.getCalendar());
+        julian_date.addText("mein Tag", "de");
+        julian_date.setCalendar(MCRCalendar.TAG_GREGORIAN);
+        
+        MCRMetaHistoryDate gregorian_date = new MCRMetaHistoryDate("subtag","type",0);
+        gregorian_date.setCalendar(MCRCalendar.TAG_GREGORIAN);
+        gregorian_date.setVonDate("06.03.1964", gregorian_date.getCalendar());
+        gregorian_date.setBisDate("06.03.1964", gregorian_date.getCalendar());
+        gregorian_date.addText("mein Tag", "de");
+        
+        Element julian_date_xml = julian_date.createXML();
+        Element gregorian_date_xml = gregorian_date.createXML();
+        assertTrue("XML elements should be equal", MCRXMLHelper.deepEqual(julian_date_xml, gregorian_date_xml));
+        
+        MCRMetaHistoryDate julian_date_read = new MCRMetaHistoryDate();
+        julian_date_read.setFromDOM(julian_date_xml);
+        MCRMetaHistoryDate gregorian_date_read = new MCRMetaHistoryDate();
+        gregorian_date_read.setFromDOM(gregorian_date_xml);
+        assertEquals("read objects from XML should be equal", julian_date_read, gregorian_date_read);
+        
+        MCRMetaHistoryDate gregorian_date_clone = gregorian_date_read.clone();
+        assertEquals("cloned object should be equal with original", gregorian_date_read, gregorian_date_clone);
+   }
+    
+}
---
+++ mycore-mods/src/main/java/org/mycore/mods/enrichment/MCRDataSource.java
@@ -0,0 +1,62 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mycore.mods.enrichment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A data source is able to return publication data in MODS format
+ * for a given publication identifier. A data source may support more than
+ * one identifier, e.g. DOI and PubMed ID for PubMed as a data source:
+ *
+ * MCR.MODS.EnrichmentResolver.DataSource.PubMed.IdentifierTypes=doi pubmed
+ *
+ * For each supported identifier type, the data source has a resolver that
+ * returns MODS data for that identifier type.
+ *
+ * @author Frank L\u00FCtzenkirchen
+ */
+class MCRDataSource {
+
+    private String sourceID;
+
+    private List<MCRIdentifierResolver> resolvers = new ArrayList<MCRIdentifierResolver>();
+
+    MCRDataSource(String sourceID) {
+        this.sourceID = sourceID;
+    }
+
+    void addResolver(MCRIdentifierResolver resolver) {
+        resolvers.add(resolver);
+    }
+
+    /** Returns all resolvers to get publication data for a given identifier */
+    List<MCRIdentifierResolver> getResolvers() {
+        return resolvers;
+    }
+
+    String getID() {
+        return sourceID;
+    }
+
+    public String toString() {
+        return "data source " + sourceID;
+    }
+}
