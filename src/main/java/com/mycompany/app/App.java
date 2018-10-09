b5ed702a28544dea5b75ec5684baa07b7a199404
Luc Esape <luc.esape@gmail.com>
2018-10-09 21:26:06 +0200

    Bug commit from the following repository aycagrlyk/myDemoApp This bug commit is a reflect of source code from: http://github.com/aycagrlyk/myDemoApp/commit/2611808f609e9647c077c911d1fdd5328d5f8412.

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,106 @@
+package com.mycompany.app;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
+public class App
+{
+
+    
+    public static boolean search(ArrayList <Integer> a,int sizeA,ArrayList <Integer> b,int sizeB){
+        if(sizeA!=sizeB){
+            return false;
+        }else{
+            ArrayList <Integer> temp =new ArrayList<Integer>();
+            
+            for(int i=sizeA-1;i>=0;i--){
+                temp.add(a.get(i));
+                
+            }
+            for(int j=0;j<sizeB;j++){
+                if(temp.get(j)==b.get(j)){
+                    
+                }else{
+                    return false;
+            }
+            
+        
+        }
+        return true;
+    }
+}
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+
+        get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+          //System.out.println(req.queryParams("input1"));
+          //System.out.println(req.queryParams("input2"));
+
+          String input1 = req.queryParams("input1");
+          java.util.Scanner sc1 = new java.util.Scanner(input1);
+          sc1.useDelimiter("[;\r\n]+");
+          java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+          while (sc1.hasNext())
+          {
+            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+            inputList.add(value);
+          }
+          System.out.println(inputList);
+
+
+          String input2 = req.queryParams("input2").replaceAll("\\s","");
+          int input2AsInt = Integer.parseInt(input2);
+                //
+          String input3 = req.queryParams("input3");
+          java.util.Scanner sc3 = new java.util.Scanner(input3);
+          sc3.useDelimiter("[;\r\n]+");
+          java.util.ArrayList<Integer> inputList2 = new java.util.ArrayList<>();
+          while (sc3.hasNext())
+          {
+            int value = Integer.parseInt(sc3.next().replaceAll("\\s",""));
+            inputList2.add(value);
+          }
+          System.out.println(inputList2);
+
+
+          String input4 = req.queryParams("input4").replaceAll("\\s","");
+          int input4AsInt = Integer.parseInt(input4);
+            
+
+          boolean result = App.search(inputList, input2AsInt,inputList2,input4AsInt);
+
+         Map map = new HashMap();
+          map.put("result", result);
+          return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+            (rq, rs) -> {
+              Map map = new HashMap();
+              map.put("result", "not computed yet!");
+              return new ModelAndView(map, "compute.mustache");
+            },
+            new MustacheTemplateEngine());
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+}
+
+
