a71d8234dc7f12df0b2a2db4b06bad466d8952f0
Luc Esape <luc.esape@gmail.com>
2018-10-10 01:19:52 +0200

    Bug commit from the following repository pabender/JMRI This bug commit is a reflect of source code from: http://github.com/pabender/JMRI/commit/d707cc6e22eed3a752100d0780cbfe7a793ffafd.

---
+++ java/src/jmri/jmrix/internal/configurexml/InternalTurnoutManagerXml.java
@@ -0,0 +1,40 @@
+package jmri.jmrix.internal.configurexml;
+
+import org.jdom2.Element;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Provides load and store functionality for configuring
+ * InternalTurnoutManagers.
+ * <P>
+ * Uses the store method from the abstract base class, but provides a load
+ * method here.
+ *
+ * @author Bob Jacobsen Copyright: Copyright (c) 2006
+ */
+public class InternalTurnoutManagerXml extends jmri.managers.configurexml.AbstractTurnoutManagerConfigXML {
+
+    public InternalTurnoutManagerXml() {
+        super();
+    }
+
+    @Override
+    public void setStoreElementClass(Element turnouts) {
+        turnouts.setAttribute("class", this.getClass().getName());
+    }
+
+    @Override
+    public void load(Element element, Object o) {
+        log.error("Invalid method called");
+    }
+
+    @Override
+    public boolean load(Element shared, Element perNode) {
+        // load individual turnouts
+        return loadTurnouts(shared, perNode);
+    }
+
+    private final static Logger log = LoggerFactory.getLogger(InternalTurnoutManagerXml.class);
+
+}
