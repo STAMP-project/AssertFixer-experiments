7852bbac97f8fb583284e9c4007c88088e4f8873
Luc Esape <luc.esape@gmail.com>
2018-10-12 14:21:46 +0200

    Bug commit from the following repository arquillian/arquillian-cube This bug commit is a reflect of source code from: http://github.com/arquillian/arquillian-cube/commit/f8b222bd0df015349f50f3f5c3645313505bd658.

---
+++ kubernetes/istio/istio/src/main/java/org/arquillian/cube/istio/impl/IstioClientCreator.java
@@ -0,0 +1,23 @@
+package org.arquillian.cube.istio.impl;
+
+import io.fabric8.kubernetes.clnt.v4_0.KubernetesClient;
+import me.snowdrop.istio.client.IstioClient;
+import org.jboss.arquillian.core.api.InstanceProducer;
+import org.jboss.arquillian.core.api.annotation.ApplicationScoped;
+import org.jboss.arquillian.core.api.annotation.Inject;
+import org.jboss.arquillian.core.api.annotation.Observes;
+
+public class IstioClientCreator {
+
+    @Inject
+    @ApplicationScoped
+    InstanceProducer<IstioClient> istioClientInstanceProducer;
+
+    public void createIstioClient(@Observes final KubernetesClient client) {
+
+        IstioClient istioClient = client.adapt(IstioClient.class);
+        istioClientInstanceProducer.set(istioClient);
+
+    }
+
+}
