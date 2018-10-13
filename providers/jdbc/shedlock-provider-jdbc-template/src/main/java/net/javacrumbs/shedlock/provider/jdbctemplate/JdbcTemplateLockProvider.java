4cb06184dbccf1ab54bb301b86374e10ecec3dae
Luc Esape <luc.esape@gmail.com>
2018-10-13 19:37:30 +0200

    Bug commit from the following repository lukas-krecan/ShedLock This bug commit is a reflect of source code from: http://github.com/lukas-krecan/ShedLock/commit/981cb3a3e74448574416da5a6da244804fd0a5d5.

---
+++ providers/jdbc/shedlock-provider-jdbc-template/src/main/java/net/javacrumbs/shedlock/provider/jdbctemplate/JdbcTemplateLockProvider.java
@@ -0,0 +1,151 @@
+/**
+ * Copyright 2009-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.javacrumbs.shedlock.provider.jdbctemplate;
+
+import net.javacrumbs.shedlock.support.StorageBasedLockProvider;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.security.auth.login.Configuration;
+import javax.sql.DataSource;
+import java.util.TimeZone;
+
+/**
+ * Lock provided by JdbcTemplate. It uses a table that contains lock_name and locked_until.
+ * <ol>
+ * <li>
+ * Attempts to insert a new lock record. Since lock name is a primary key, it fails if the record already exists. As an optimization,
+ * we keep in-memory track of created  lock records.
+ * </li>
+ * <li>
+ * If the insert succeeds (1 inserted row) we have the lock.
+ * </li>
+ * <li>
+ * If the insert failed due to duplicate key or we have skipped the insertion, we will try to update lock record using
+ * UPDATE tableName SET lock_until = :lockUntil WHERE name = :lockName AND lock_until &lt;= :now
+ * </li>
+ * <li>
+ * If the update succeeded (1 updated row), we have the lock. If the update failed (0 updated rows) somebody else holds the lock
+ * </li>
+ * <li>
+ * When unlocking, lock_until is set to now.
+ * </li>
+ * </ol>
+ */
+public class JdbcTemplateLockProvider extends StorageBasedLockProvider {
+
+    private static final String DEFAULT_TABLE_NAME = "shedlock";
+
+    public JdbcTemplateLockProvider(JdbcTemplate jdbcTemplate) {
+        this(jdbcTemplate, (PlatformTransactionManager) null);
+    }
+
+    public JdbcTemplateLockProvider(JdbcTemplate jdbcTemplate, PlatformTransactionManager transactionManager) {
+        this(jdbcTemplate, transactionManager, DEFAULT_TABLE_NAME);
+    }
+
+    public JdbcTemplateLockProvider(JdbcTemplate jdbcTemplate, String tableName) {
+        this(jdbcTemplate, null, tableName);
+    }
+
+    public JdbcTemplateLockProvider(DataSource dataSource) {
+        this(new JdbcTemplate(dataSource));
+    }
+
+    public JdbcTemplateLockProvider(DataSource dataSource, String tableName) {
+        this(new JdbcTemplate(dataSource), tableName);
+    }
+
+    public JdbcTemplateLockProvider(JdbcTemplate jdbcTemplate, PlatformTransactionManager transactionManager, String tableName) {
+        this(Configuration.builder()
+            .withJdbcTemplate(jdbcTemplate)
+            .withTransactionManager(transactionManager)
+            .withTableName(tableName)
+            .build()
+        );
+    }
+
+    public JdbcTemplateLockProvider(Configuration configuration) {
+        super(new JdbcTemplateStorageAccessor(configuration));
+    }
+
+    public static class Configuration {
+        private final JdbcTemplate jdbcTemplate;
+        private final PlatformTransactionManager transactionManager;
+        private final String tableName;
+        private final TimeZone timeZone;
+
+        Configuration(JdbcTemplate jdbcTemplate, PlatformTransactionManager transactionManager, String tableName, TimeZone timeZone) {
+            this.jdbcTemplate = jdbcTemplate;
+            this.transactionManager = transactionManager;
+            this.tableName = tableName;
+            this.timeZone = timeZone;
+        }
+
+        public JdbcTemplate getJdbcTemplate() {
+            return jdbcTemplate;
+        }
+
+        public PlatformTransactionManager getTransactionManager() {
+            return transactionManager;
+        }
+
+        public String getTableName() {
+            return tableName;
+        }
+
+        public TimeZone getTimeZone() {
+            return timeZone;
+        }
+
+        public static Configuration.Builder builder() {
+            return new Configuration.Builder();
+        }
+
+        public static class Builder {
+            private JdbcTemplate jdbcTemplate;
+            private PlatformTransactionManager transactionManager;
+            private String tableName= DEFAULT_TABLE_NAME;
+            private TimeZone timeZone;
+
+            public Builder withJdbcTemplate(JdbcTemplate jdbcTemplate) {
+                this.jdbcTemplate = jdbcTemplate;
+                return this;
+            }
+
+            public Builder withTransactionManager(PlatformTransactionManager transactionManager) {
+                this.transactionManager = transactionManager;
+                return this;
+            }
+
+            public Builder withTableName(String tableName) {
+                this.tableName = tableName;
+                return this;
+            }
+
+            public Builder withTimeZone(TimeZone timeZone) {
+                this.timeZone = timeZone;
+                return this;
+            }
+
+            public JdbcTemplateLockProvider.Configuration build() {
+                return new JdbcTemplateLockProvider.Configuration(jdbcTemplate, transactionManager, tableName, timeZone);
+            }
+        }
+
+    }
+
+}
---
+++ providers/zookeeper/shedlock-provider-zookeeper-curator/src/main/java/net/javacrumbs/shedlock/provider/zookeeper/curator/ZookeeperCuratorLockProvider.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright 2009-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.javacrumbs.shedlock.provider.zookeeper.curator;
+
+import net.javacrumbs.shedlock.core.LockConfiguration;
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.core.SimpleLock;
+import net.javacrumbs.shedlock.support.LockException;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.utils.PathUtils;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+
+import java.time.Instant;
+import java.util.Optional;
+
+import static java.time.Instant.now;
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Locks kept using ZooKeeper. When locking, creates an ephemeral node with node name = lock name, when unlocking, removes the node.
+ */
+public class ZookeeperCuratorLockProvider implements LockProvider {
+    public static final String DEFAULT_PATH = "/shedlock";
+    private final String path;
+    private final CuratorFramework client;
+
+    public ZookeeperCuratorLockProvider(CuratorFramework client) {
+        this(client, DEFAULT_PATH);
+    }
+
+    public ZookeeperCuratorLockProvider(CuratorFramework client, String path) {
+        this.client = requireNonNull(client);
+        this.path = PathUtils.validatePath(path);
+    }
+
+    @Override
+    public Optional<SimpleLock> lock(LockConfiguration lockConfiguration) {
+        Instant now = now();
+        if (lockConfiguration.getLockAtLeastUntil().isAfter(now)) {
+            throw new UnsupportedOperationException("ZookeeperCuratorLockProvider does not support nonzero lockAtLeastUntil yet.");
+        }
+        try {
+            String nodePath = getNodePath(lockConfiguration);
+            client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).forPath(nodePath);
+            return Optional.of(new CuratorLock(nodePath, client));
+        } catch (KeeperException.NodeExistsException ex) {
+            return Optional.empty();
+        } catch (Exception e) {
+            throw new LockException("Can not create node", e);
+        }
+    }
+
+    private String getNodePath(LockConfiguration lockConfiguration) {
+        return path + "/" + lockConfiguration.getName();
+    }
+
+    private static final class CuratorLock implements SimpleLock {
+        private final String nodePath;
+        private final CuratorFramework client;
+
+        private CuratorLock(String nodePath, CuratorFramework client) {
+            this.nodePath = nodePath;
+            this.client = client;
+        }
+
+        @Override
+        public void unlock() {
+            try {
+                client.delete().forPath(nodePath);
+            } catch (Exception e) {
+                throw new LockException("Can not remove node", e);
+            }
+        }
+    }
+}
