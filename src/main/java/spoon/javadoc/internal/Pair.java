4f9dcee2cd405fabcd701694fd555d73222acae7
Luc Esape <luc.esape@gmail.com>
2018-10-14 16:22:49 +0200

    Bug commit from the following repository INRIA/spoon This bug commit is a reflect of source code from: http://github.com/INRIA/spoon/commit/37ae1c3ad070cebaa50f41d5873eaac60f7db58a.

---
+++ src/main/java/spoon/javadoc/internal/Pair.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2006-2018 INRIA and contributors
+ * Spoon - http://spoon.gforge.inria.fr/
+ *
+ * This software is governed by the CeCILL-C License under French law and
+ * abiding by the rules of distribution of free software. You can use, modify
+ * and/or redistribute the software under the terms of the CeCILL-C license as
+ * circulated by CEA, CNRS and INRIA at http://www.cecill.info.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the CeCILL-C License for more details.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-C license and that you accept its terms.
+ */
+
+package spoon.javadoc.internal;
+
+
+/**
+ * Simply a pair of objects.
+ *
+ * @param <A> type of object a.
+ * @param <B> type of object b.
+ */
+public class Pair<A, B> {
+    public final A a;
+    public final B b;
+
+    public Pair(A a, B b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+
+        if (a != null ? !a.equals(pair.a) : pair.a != null) return false;
+        if (b != null ? !b.equals(pair.b) : pair.b != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = a != null ? a.hashCode() : 0;
+        return 31 * result + (b != null ? b.hashCode() : 0);
+    }
+
+}
